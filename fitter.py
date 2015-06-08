11f7e129be8e106197b7bd730832fec25052aedb
goracle <goracle@users.noreply.github.com>
2015-06-08 16:20:43 -0400

    Pylint correction

--- fitter.py
+++ fitter.py
@@ -4,15 +4,15 @@
 
 #plotting part
 
-import matplotlib.pyplot as plt
-import re
+#import matplotlib.pyplot as plt
+#import re
 import sys
 import getopt
-from numbers import Number
-from decimal import Decimal
-from fractions import Fraction
-from numpy import array
-from numpy import linalg
+#from numbers import Number
+#from decimal import Decimal
+#from fractions import Fraction
+#from numpy import array
+#from numpy import linalg
 from numpy.linalg import inv
 from collections import defaultdict
 
@@ -24,7 +24,7 @@ def main(argv):
     Give usage information or set the input file.
     """
     try:
-        opts, args = getopt.getopt(argv,"hi:",["ifile="])
+        opts = getopt.getopt(argv, "hi:", ["ifile="])[0]
     except getopt.GetoptError:
         print "File not found"
         print "usage:", sys.argv[0], "-i <inputfile>"
@@ -33,47 +33,49 @@ def main(argv):
         if opt == '-h':
             print "usage:", sys.argv[0], "-i <inputfile>"
             sys.exit()
-        elif opt in ("-i" "--ifile"):
+        elif opt in "-i" "--ifile":
             return arg
 
 def tree():
     """Used to make a multidimensional dict"""
     return defaultdict(tree)
 
-        
 #main part
-if __name__ == "__main__": 
+if __name__ == "__main__":
     #re.match(r'',input part from file)
     try:
-        inputfile = main(sys.argv[1:]) 
-        input = open(inputfile,"r")
-    except (IOError,TypeError):
-        print "File:", inputfile, "not found"
+        INPUTFILE = main(sys.argv[1:])
+        IFILE = open(INPUTFILE, "r")
+    except (IOError, TypeError):
+        print "File:", INPUTFILE, "not found"
         print "usage:", sys.argv[0], "-i <inputfile>"
         sys.exit(2)
-    coords=[]
-    dict=tree()
-    with open(inputfile) as f:
+    COORDS = []
+    CDICT = tree()
+    with open(INPUTFILE) as f:
         for line in f:
             try:
-                cols = [float(x) for x in line.split()] 
-            except (ValueError):
+                cols = [float(x) for x in line.split()]
+            except ValueError:
+                print "ignored line: '", line, "'"
                 continue
-            #make a list of x y coords
-            if len(cols)==2:
-                coords.append([cols[0],cols[1]])
+            #make a list of x y COORDS
+            if len(cols) == 2:
+                COORDS.append([cols[0], cols[1]])
             #Find the C_ij^-1 inverse covariance matrix
             else:
-                dict[cols[0]][cols[1]]=cols[2]
+                CDICT[cols[0]][cols[1]] = cols[2]
             #matchObj=re.search(r'(.*?) +(.*?)',line)
             #if matchObj:
             #    print "gr", matchObj.group(1)
             #    print "ga", matchObj.group(2)
             #print line
-    #build the covariance matrix from the dict
-    cov=array([[dict[coords[i][0]][coords[j][0]]  for i in range(len(coords))] for j in range(len(coords))])
-    print cov[2,3]
-    covinv=inv(cov)
-    print covinv[2,2]
-    print len(coords)
+    #build the covariance matrix from the CDICT
+    COV = [[CDICT[COORDS[i][0]][COORDS[j][0]]
+            for i in range(len(COORDS))]
+           for j in range(len(COORDS))]
+    print COV[2][2]
+    COVINV = inv(COV)
+    print COVINV[2][2]
+    print len(COORDS)
     sys.exit()
