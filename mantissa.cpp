4f584be320cbe5e6b767ab3979c1b39d5d066f4a
goracle <goracle@users.noreply.github.com>
2015-06-09 02:42:54 -0400

    Computes the difference between summing up and summing down, harmonic series

--- mantissa.cpp
+++ mantissa.cpp
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <math.h>
+#include <time.h>
 
 using namespace std;
 
@@ -15,8 +17,26 @@ static void show_usage(std::string name)
 	    << "\n";
 }
 
+double difference(long N)
+{
+  double differ=0;
+  double sumU=0;
+  double sumD=0;
+  for(int i=1; i<N+1; i++)
+    {
+      sumU+=1.0/i;
+    }
+  for(int i=N; i>0; i--)
+    {
+      sumD+=1.0/i;
+    }
+  differ=1.0/2.0*fabs((sumU-sumD))/(fabs(sumU)+fabs(sumD));
+  return differ;
+}
+
 int main(int argc, char* argv[])
 {
+  clock_t tStart = clock();
   if (argc != 2)
     {
       show_usage(argv[0]);
@@ -39,8 +59,16 @@ int main(int argc, char* argv[])
     {
       sumdown+=1.0/i;
     }
-  cout << "sum down(" << N << ") = " << sumdown << "\n";
+  cout << "sum down (" << N << ") = " << sumdown << "\n";
   cout << "sum up (" << N << ") = " << sumup << "\n";
+  //Plot part
+  //Gnuplot gp2;
+  //std::vector<boost::tuple<double, double> > xy_pts_A1;
+  for(double i=0; i<4; i++)
+    {
+      cout << "pow=" << i << " : " << difference(N*pow(10.0,i)) << "\n";
+    }
+  printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
   return 0;
 }
 	 
