4a65fc11757b8252699ca8a69077118265998c77
Dan H <goracle@users.noreply.github.com>
2017-06-27 07:12:53 -0400

    Raise roundoff error of chi^2 to increase speed.

--- latfit/mathfun/chi_sq.py
+++ latfit/mathfun/chi_sq.py
@@ -1,4 +1,3 @@
-from math import fsum
 from numpy import dot
 import numpy as np
 from latfit.config import fit_func
@@ -7,4 +6,4 @@ def chi_sq(trial_params, covinv, coords):
     """Compute chi^2 given a set of trial parameters,
     the inverse covariance matrix, and the x-y coordinates to fit.
     """
-    return fsum([dot(dot((np.array(coords[outer][1])- fit_func(coords[outer][0], trial_params)),covinv[outer][inner]),(np.array(coords[inner][1])-fit_func(coords[inner][0],trial_params))) for outer in range(len(coords)) for inner in range(len(coords))])
+    return np.sum([dot(dot((coords[outer][1]- fit_func(coords[outer][0], trial_params)),covinv[outer][inner]),(coords[inner][1]-fit_func(coords[inner][0],trial_params))) for outer in range(len(coords)) for inner in range(len(coords))])
