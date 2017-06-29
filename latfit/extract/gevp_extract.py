d4efc7900c68d19958e827b7018eae18adb9a5b4
Dan H <goracle@users.noreply.github.com>
2017-06-29 10:41:34 -0400

    Add bug fixes. Make variable names more uniform.

--- latfit/extract/gevp_extract.py
+++ latfit/extract/gevp_extract.py
@@ -9,15 +9,16 @@ from latfit.extract.proc_folder import proc_folder
 from latfit.extract.proc_file import proc_line
 
 from latfit.config import GEVP_DIRS
+from latfit.config import EFF_MASS
 
-def gevp_extract(XMIN,XMAX,XSTEP):
+def gevp_extract(xmin,xmax,XSTEP):
     i = 0
     #result is returned as a named tuple: RESRET
     RESRET = namedtuple('ret', ['coord', 'covar'])
-    #Reuse results
+    #Reuse results (store all read-in data)
     REUSE={xmin:0}
     #dimcov is dimensions of the covariance matrix
-    dimcov = int((XMAX-XMIN)/XSTEP+1)
+    dimcov = int((xmax-xmin)/XSTEP+1)
     #dimops is the dimension of the correlator matrix
     dimops = len(GEVP_DIRS)
     #cov is the covariance matrix
@@ -25,7 +26,7 @@ def gevp_extract(XMIN,XMAX,XSTEP):
     #COORDS are the coordinates to be plotted.
     #the ith point with the jth value
     COORDS = np.zeros((dimcov,2),dtype=object)
-    for timei in np.arange(XMIN, XMAX+1, XSTEP):
+    for timei in np.arange(xmin, xmax+1, XSTEP):
         if timei in REUSE:
             REUSE['i']=REUSE[timei]
         else:
@@ -35,40 +36,40 @@ def gevp_extract(XMIN,XMAX,XSTEP):
                 print("***ERROR***")
                 print("Time slice:",timei,", is not being stored for some reason")
                 sys.exit(1)
-        timei2=ceil(float(timei)/2.0/XSTEP)*XSTEP if ceil(float(timei)/2.0)!=timei else max(floor(float(timei)/2.0/XSTEP)*XSTEP,XMIN)
+        timei2=ceil(float(timei)/2.0/XSTEP)*XSTEP if ceil(float(timei)/2.0)!=timei else max(floor(float(timei)/2.0/XSTEP)*XSTEP,xmin)
         #set the times coordinate
         COORDS[i][0] = timei
         #extract files
         IFILES = [[proc_folder(GEVP_DIRS[op1][op2],timei) for op1 in range(dimops)] for op2 in range(dimops)]
-        IFILES2 = [[proc_folder(GEVP_DIRS[op1][op2],timei2) for op1 in range(dimops)] for op2 in range(dimops)]
+        I2FILES = [[proc_folder(GEVP_DIRS[op1][op2],timei2) for op1 in range(dimops)] for op2 in range(dimops)]
         #check for errors
         IFILES = [[pre_proc_file(IFILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
-        IFILES2 = [[pre_proc_file(IFILES2[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
+        I2FILES = [[pre_proc_file(I2FILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
         if EFF_MASS:
-            IFILES3 = [[proc_folder(GEVP_DIRS[op1][op2],timei+XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
-            IFILES4 = [[proc_folder(GEVP_DIRS[op1][op2],timei+2*XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
-            IFILES3 = [[pre_proc_file(IFILES3[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
-            IFILES4 = [[pre_proc_file(IFILES4[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
+            I3FILES = [[proc_folder(GEVP_DIRS[op1][op2],timei+XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
+            I4FILES = [[proc_folder(GEVP_DIRS[op1][op2],timei+2*XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
+            I3FILES = [[pre_proc_file(I3FILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
+            I4FILES = [[pre_proc_file(I4FILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
         j=0
-        for timej in np.arange(XMIN, XMAX+1, XSTEP):
+        for timej in np.arange(xmin, xmax+1, XSTEP):
             if timej in REUSE:
                 REUSE['j']=REUSE[timej]
             else:
                 REUSE['j']=0
-            timej2=ceil(float(timej)/2.0/XSTEP)*XSTEP if ceil(float(timej)/2.0)!=timej else max(floor(float(timej)/2.0/XSTEP)*XSTEP,XMIN)
+            timej2=ceil(float(timej)/2.0/XSTEP)*XSTEP if ceil(float(timej)/2.0)!=timej else max(floor(float(timej)/2.0/XSTEP)*XSTEP,xmin)
             TIME_ARR=[timei,timei2,timej,timej2,XSTEP]
             JFILES = [[proc_folder(GEVP_DIRS[op1][op2],timej) for op1 in range(dimops)] for op2 in range(dimops)]
             JFILES = [[pre_proc_file(JFILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
-            JFILES2 = [[proc_folder(GEVP_DIRS[op1][op2],timej2) for op1 in range(dimops)] for op2 in range(dimops)]
-            JFILES2 = [[pre_proc_file(JFILES2[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
+            J2FILES = [[proc_folder(GEVP_DIRS[op1][op2],timej2) for op1 in range(dimops)] for op2 in range(dimops)]
+            J2FILES = [[pre_proc_file(J2FILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
             if EFF_MASS:
-                JFILES3 = [[proc_folder(GEVP_DIRS[op1][op2],timej+XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
-                JFILES4 = [[proc_folder(GEVP_DIRS[op1][op2],timej+2*XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
-                JFILES3 = [[pre_proc_file(JFILES3[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
-                JFILES4 = [[pre_proc_file(JFILES4[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
-                RESRET = gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,[(I3FILES,J3FILES),(I4FILES,J4FILES)],reuse=REUSE)
+                J3FILES = [[proc_folder(GEVP_DIRS[op1][op2],timej+XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
+                J4FILES = [[proc_folder(GEVP_DIRS[op1][op2],timej+2*XSTEP) for op1 in range(dimops)] for op2 in range(dimops)]
+                J3FILES = [[pre_proc_file(J3FILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
+                J4FILES = [[pre_proc_file(J4FILES[op1][op2],GEVP_DIRS[op1][op2]) for op1 in range(dimops)] for op2 in range(dimops)]
+                RESRET = gevp_proc(IFILES,I2FILES,JFILES,J2FILES,TIME_ARR,[(I3FILES,J3FILES),(I4FILES,J4FILES)],reuse=REUSE)
             else:
-                RESRET = gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,reuse=REUSE)
+                RESRET = gevp_proc(IFILES,I2FILES,JFILES,J2FILES,TIME_ARR,reuse=REUSE)
             COV[i][j] = RESRET.covar
             REUSE[timej]=RESRET.returnblk
             if timei==timej:
--- latfit/extract/gevp_proc.py
+++ latfit/extract/gevp_proc.py
@@ -9,11 +9,13 @@ from warnings import warn
 from itertools import chain
 
 from latfit.mathfun.proc_MEFF import proc_MEFF
+from latfit.extract.proc_line import proc_line
 
 from latfit.config import UNCORR
+from latfit.config import EFF_MASS
 
 CSENT = object()
-def gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,extra_pairs=[(CSENT,CSENT),(CSENT,CSENT)],reuse={}):
+def gevp_proc(IFILES,I2FILES,JFILES,J2FILES,TIME_ARR,extra_pairs=[(CSENT,CSENT),(CSENT,CSENT)],reuse={}):
     #setup global values
     rets = namedtuple('rets', ['coord', 'covar', 'returnblk'])
     dimops=len(IFILES)
@@ -25,7 +27,7 @@ def gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,extra_pairs=[(CSENT,CSENT),
         J3FILES=extra_pairs[0][1]
         I4FILES=extra_pairs[1][0]
         J4FILES=extra_pairs[1][1]
-        if I3FILES == CSENT or J3FILES:
+        if I3FILES == CSENT or J3FILES == CSENT:
             print("***ERROR***")
             print("Missing time adjacent file(s).")
             sys.exit(1)
@@ -54,15 +56,15 @@ def gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,extra_pairs=[(CSENT,CSENT),
             for opa in range(dimops):
                 for opb in range(dimops):
                     CI_LHS[opa][opb]=proc_line(getline(IFILES[opa][opb],config+1),IFILES[opa][opb])
-                    CI_RHS[opa][opb]=proc_line(getline(IFILES2[opa][opb],config+1),IFILES2[opa][opb])
+                    CI_RHS[opa][opb]=proc_line(getline(I2FILES[opa][opb],config+1),I2FILES[opa][opb])
                     if EFF_MASS:
-                        CIP_LHS[opa][opb]=proc_line(getline(IFILES3[opa][opb],config+1),IFILES3[opa][opb])
-                        CIPP_LHS[opa][opb]=proc_line(getline(IFILES4[opa][opb],config+1),IFILES4[opa][opb])
+                        CIP_LHS[opa][opb]=proc_line(getline(I3FILES[opa][opb],config+1),I3FILES[opa][opb])
+                        CIPP_LHS[opa][opb]=proc_line(getline(I4FILES[opa][opb],config+1),I4FILES[opa][opb])
             if EFF_MASS:
                 eigvalsI,eigvecsI=eig(CI_LHS,CI_RHS,overwrite_a=True,check_finite=False)
                 eigvalsIP,eigvecsIP=eig(CIP_LHS,CI_RHS,overwrite_a=True,check_finite=False)
                 eigvalsIPP,eigvecsIPP=eig(CIPP_LHS,CI_RHS,overwrite_a=True,overwrite_b=True,check_finite=False)
-                reuse['i'][config]=np.array([proc_MEFF(eigvalsI[op],eigvalsIP[op],eigvalsIPP[op],ifiles_chk[op,:]) for op in range(dimops)])
+                reuse['i'][config]=np.array([proc_MEFF(eigvalsI[op].real,eigvalsIP[op].real,eigvalsIPP[op].real) for op in range(dimops)])
             else:
                 reuse['i'][config],eigvecsI=eig(CI_LHS,CI_RHS,overwrite_a=True,overwrite_b=True,check_finite=False)
     avgI=np.mean(reuse['i'],axis=0)
@@ -74,8 +76,8 @@ def gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,extra_pairs=[(CSENT,CSENT),
             print("***ERROR***")
             print("GEVP has negative eigenvalues.")
             sys.exit(1)
-    if (IFILES==JFILES).all():
-        retblk=reuse['i']
+    if np.array_equal(IFILES,JFILES):
+        reuse['j']=reuse['i']
     try:
         if not num_configs==len(reuse['j']):
             print("***ERROR***")
@@ -88,17 +90,17 @@ def gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,extra_pairs=[(CSENT,CSENT),
             for opa in range(dimops):
                 for opb in range(dimops):
                     CJ_LHS[opa][opb]=proc_line(getline(JFILES[opa][opb],config+1),JFILES[opa][opb])
-                    CJ_RHS[opa][opb]=proc_line(getline(JFILES2[opa][opb],config+1),JFILES2[opa][opb])
+                    CJ_RHS[opa][opb]=proc_line(getline(J2FILES[opa][opb],config+1),J2FILES[opa][opb])
                     if EFF_MASS:
-                        CJP_LHS[opa][opb]=proc_line(getline(JFILES3[opa][opb],config+1),JFILES3[opa][opb])
-                        CJPP_LHS[opa][opb]=proc_line(getline(JFILES4[opa][opb],config+1),JFILES4[opa][opb])
+                        CJP_LHS[opa][opb]=proc_line(getline(J3FILES[opa][opb],config+1),J3FILES[opa][opb])
+                        CJPP_LHS[opa][opb]=proc_line(getline(J4FILES[opa][opb],config+1),J4FILES[opa][opb])
             if EFF_MASS:
                 eigvalsJ,eigvecsJ=eig(CJ_LHS,CJ_RHS,overwrite_a=True,check_finite=False)
                 eigvalsJP,eigvecsJP=eig(CJP_LHS,CJ_RHS,overwrite_a=True,check_finite=False)
                 eigvalsJPP,eigvecsJPP=eig(CJPP_LHS,CJ_RHS,overwrite_a=True,overwrite_b=True,check_finite=False)
-                reuse['i'][config]=np.array([proc_MEFF(eigvalsJ[op],eigvalsJP[op],eigvalsJPP[op],jfiles_chk[op,:]) for op in range(dimops)])
+                reuse['j'][config]=np.array([proc_MEFF(eigvalsJ[op].real,eigvalsJP[op].real,eigvalsJPP[op].real) for op in range(dimops)])
             else:
-                reuse['i'][config],eigvecsJ=eig(CJ_LHS,CJ_RHS,overwrite_a=True,overwrite_b=True,check_finite=False)
+                reuse['j'][config],eigvecsJ=eig(CJ_LHS,CJ_RHS,overwrite_a=True,overwrite_b=True,check_finite=False)
     avgJ=np.mean(reuse['j'],axis=0)
     for test in avgJ:
         if test.imag != 0:
@@ -107,8 +109,8 @@ def gevp_proc(IFILES,IFILES2,JFILES,JFILES2,TIME_ARR,extra_pairs=[(CSENT,CSENT),
             sys.exit(1)
     if UNCORR:
         for a in range(dimops):
-            coventry[a][a]=sum([(avgI[a]-reuse['i'][k][a])*(avgI[a]-reuse['i'][k][a]) for k in range(num_configs)],axis=0)
+            coventry[a][a]=np.sum([(avgI[a]-reuse['i'][k][a])*(avgI[a]-reuse['i'][k][a]) for k in range(num_configs)],axis=0)
     else:
-        coventry=sum([np.outer((avgI-reuse['i'][k]),(avgJ-reuse['j'][k])) for k in range(num_configs)],axis=0)
+        coventry=np.sum([np.outer((avgI-reuse['i'][k]),(avgJ-reuse['j'][k])) for k in range(num_configs)],axis=0)
     return rets(coord=avgI, covar=coventry,returnblk=reuse['j'])
 gevp_proc.CONFIGSENT = object()
