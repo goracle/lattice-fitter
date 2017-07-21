e85a08aa984c043d7c6961fbbfd5c3c37aa94969
goracle <goracle@users.noreply.github.com>
2017-07-21 13:34:45 -0400

    Update __init__'s.

--- latfit/mathfun/__init__.py
+++ latfit/mathfun/__init__.py
@@ -1,3 +1,4 @@
 """Math/analytic functions.  Typically no i/o."""
 
-__all__ = ["chi_sq", "proc_meff", "elim_jkconfigs", "maptomat", "covinv_avg"]
+__all__ = ["chi_sq", "proc_meff", "binconf",
+           "elim_jkconfigs", "maptomat", "covinv_avg"]
