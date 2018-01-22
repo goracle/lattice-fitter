574891544fd4901dd76400350b25d49a1c4478da
Dan H <goracle@users.noreply.github.com>
2018-01-21 21:28:15 -0500

    Remove redundancy in extraction.

--- latfit/singlefit.py
+++ latfit/singlefit.py
@@ -29,9 +29,18 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
     inputexists(input_f)
 
     ####process the file(s)
-    coords, cov, reuse = extract(input_f, fitrange[0], fitrange[1], xstep)
+    coords_full, cov_full, reuse = extract(
+        input_f, xmin, xmax, xstep)
     #do this so reuse goes from reuse[time][config] to more convenient reuse[config][time]
 
+    ##Now that we have the data to fit, do pre-proccess it
+    params = namedtuple('fit_params', ['dimops', 'num_configs', 'prefactor'])
+    params = get_fit_params(cov_full, reuse, xmin)
+
+    #correct covariance matrix for jackknife factor
+    cov_full *= params.prefactor
+
+    #debug branch
     if PRINT_CORR:
         print(coords)
         if GEVP:
@@ -40,16 +49,17 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
             print([sqrt(cov[i][i]) for i in range(len(cov))])
         sys.exit(0)
 
-    ##Now that we have the data to fit, do pre-proccess it
-    params = namedtuple('fit_params', ['dimops', 'num_configs', 'prefactor'])
-    params = get_fit_params(cov, reuse, fitrange[0])
-
+    #select part of data to fit
+    start_index = int((fitrange[0]-xmin)/xstep)
+    stop_index = int(len(coords_full)-1-(xmax-fitrange[1])/xstep)
     time_range = np.arange(fitrange[0], fitrange[1]+1, xstep)
-    #reuse = swap(reuse, 0, 1)
+
+    coords = coords_full[start_index:stop_index+1]
+    cov = cov_full[start_index:stop_index+1, start_index:stop_index+1]
+    #reuse = swap(reuse, 0, 1), turn it into an array
     reuse = np.array([[reuse[time][config]
                        for time in time_range]
                       for config in range(params.num_configs)])
-    cov *= params.prefactor
 
     #error handling for Degrees of Freedom <= 0 (it should be > 0).
     #number of points plotted = len(cov).
@@ -63,14 +73,12 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
         covinv = covinv_avg(cov, params.dimops)
 
     if GEVP:
-        print("(Rough) scale of errors in data points = ", np.diag(sqrt(cov[0][0])))
+        print("(Rough) scale of errors in data points = ",
+              np.diag(sqrt(cov[0][0])))
     else:
         print("(Rough) scale of errors in data points = ", sqrt(cov[0][0]))
 
     if FIT:
-        #comment out options{...}, bounds for L-BFGS-B
-        ###start minimizer
-        #result_min = namedtuple('min', ['x', 'fun', 'status', 'err_in_chisq'])
         if JACKKNIFE_FIT and JACKKNIFE == 'YES':
             result_min, param_err = jackknife_fit(params, reuse, coords, time_range, covinv)
         else:
@@ -78,11 +86,11 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
             ####compute errors 8ab, print results (not needed for plot part)
             param_err = geterr(result_min, covinv, coords)
 
-        #extend the plot range as necessary
+        #use a consistent error bar scheme;
+        #if fitrange isn't max use conventional,
+        #otherwise use the new double jackknife estimate
         if xmin != fitrange[0] or xmax != fitrange[1]:
-            print("reprocessing files to extend the plot range")
-            coords, cov , _ = extract(input_f, xmin, xmax, xstep)
             result_min.error_bars = None
-        return result_min, param_err, coords, cov
+        return result_min, param_err, coords_full, cov_full
     else:
         return coords, cov
