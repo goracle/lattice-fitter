587eb221864b2a8dec9659fb910c08fe44a77290
Dan H <goracle@users.noreply.github.com>
2018-01-21 23:30:06 -0500

    Make code neater -- singlefit.py.

--- latfit/singlefit.py
+++ latfit/singlefit.py
@@ -29,9 +29,7 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
     inputexists(input_f)
 
     ####process the file(s)
-    coords_full, cov_full, reuse = extract(
-        input_f, xmin, xmax, xstep)
-    #do this so reuse goes from reuse[time][config] to more convenient reuse[config][time]
+    coords_full, cov_full, reuse = extract(input_f, xmin, xmax, xstep)
 
     ##Now that we have the data to fit, do pre-proccess it
     params = namedtuple('fit_params', ['dimops', 'num_configs',
@@ -43,24 +41,20 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
 
     #debug branch
     if PRINT_CORR:
-        print(coords)
+        print(coords_full)
         if GEVP:
-            print([sqrt(np.diag(cov[i][i])) for i in range(len(cov))])
+            print([sqrt(np.diag(cov_full[i][i])) for i in range(
+                len(cov_full))])
         else:
-            print([sqrt(cov[i][i]) for i in range(len(cov))])
+            print([sqrt(cov_full[i][i]) for i in range(len(cov_full))])
         sys.exit(0)
 
-    #select part of data to fit
-    start_index = int((fitrange[0]-xmin)/xstep)
-    stop_index = int(len(coords_full)-1-(xmax-fitrange[1])/xstep)
-    time_range = np.arange(fitrange[0], fitrange[1]+1, xstep)
+    #select subset of data for fit
+    selection = index_select(xmin, xmax, xstep, fitrange, coords_full)
+    coords, cov = fit_select(coords_full, cov_full, selection)
 
-    coords = coords_full[start_index:stop_index+1]
-    cov = cov_full[start_index:stop_index+1, start_index:stop_index+1]
-    #reuse = swap(reuse, 0, 1), turn it into an array
-    reuse = np.array([[reuse[time][config]
-                       for time in time_range]
-                      for config in range(params.num_configs)])
+    #make reuse into an array, rearrange
+    reuse = rearrange_reuse_dict(params, reuse)
 
     #error handling for Degrees of Freedom <= 0 (it should be > 0).
     #number of points plotted = len(cov).
@@ -69,9 +63,6 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
 
     ###we have data 6ab
     #at this point we have the covariance matrix, and coordinates
-    #compute inverse of covariance matrix
-    if FIT:
-        covinv = covinv_avg(cov, params.dimops)
 
     if GEVP:
         print("(Rough) scale of errors in data points = ",
@@ -80,8 +71,11 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
         print("(Rough) scale of errors in data points = ", sqrt(cov[0][0]))
 
     if FIT:
+        #compute inverse of covariance matrix
+        covinv = covinv_avg(cov, params.dimops)
         if JACKKNIFE_FIT and JACKKNIFE == 'YES':
-            result_min, param_err = jackknife_fit(params, reuse, coords, time_range, covinv)
+            result_min, param_err = jackknife_fit(params, reuse,
+                                                  coords, covinv)
         else:
             result_min = mkmin(covinv, coords)
             ####compute errors 8ab, print results (not needed for plot part)
@@ -91,7 +85,37 @@ def singlefit(input_f, fitrange, xmin, xmax, xstep):
         #if fitrange isn't max use conventional,
         #otherwise use the new double jackknife estimate
         if xmin != fitrange[0] or xmax != fitrange[1]:
-            result_min.error_bars = None
+            try:
+                result_min.error_bars = None
+            except AttributeError:
+                pass
         return result_min, param_err, coords_full, cov_full
     else:
         return coords, cov
+
+def index_select(xmin, xmax, xstep, fitrange, coords_full):
+    """Get the starting and ending indices
+    for the fitted subset of the data"""
+    start_index = int((fitrange[0]-xmin)/xstep)
+    stop_index = int(len(coords_full)-1-(xmax-fitrange[1])/xstep)
+    return start_index, stop_index
+
+def fit_select(coords_full, cov_full, selection):
+    """Select portion of data to fit with"""
+    #select part of data to fit
+    start_index = selection[0]
+    stop_index = selection[1]
+    coords = coords_full[start_index:stop_index+1]
+    cov = cov_full[start_index:stop_index+1, start_index:stop_index+1]
+    return coords, cov
+
+#do this so reuse goes from reuse[time][config] to more convenient reuse[config][time]
+def rearrange_reuse_dict(params, reuse):
+    """reuse = swap(reuse, 0, 1), turn it into an array
+    detail:
+    make reuse, the original unjackknifed data,
+    into a numpy array, swap indices
+    """
+    return np.array([[reuse[time][config]
+                      for time in params.time_range]
+                     for config in range(params.num_configs)])
