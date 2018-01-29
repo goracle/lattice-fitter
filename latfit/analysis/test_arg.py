1e3eb4f4b512a9e5699f8522abb54a318f0790f4
Dan H <goracle@users.noreply.github.com>
2018-01-28 20:39:01 -0500

    Externalize error checking for eff. mass.

--- latfit/analysis/test_arg.py
+++ latfit/analysis/test_arg.py
@@ -1,5 +1,9 @@
 """Test argument to log"""
+import sys
 from warnings import warn
+import numpy as np
+
+SENT = object()
 
 def test_arg(arg, sent=None):
     """Test if arg to log is less than zero (imaginary mass)
@@ -12,3 +16,27 @@ def test_arg(arg, sent=None):
               "calc is less than 0:", arg)
         return False
     return True
+
+def zero_p(corr1, corr2=None, times=None):
+    """Check to see if denominator of effective mass equation is 0."""
+    errlevel = 0
+    if corr1 is None and np.array_equal(corr2, np.zeros(corr2.shape)):
+        errlevel = 1
+    elif np.array_equal(corr1, corr2):
+        errlevel = 2
+    if errlevel:
+        corrs = (corr1, corr2)
+        for i in range(errlevel):
+            print("corrs["+str(i)+"] = ", corrs[i])
+        if times is not None:
+            print("problematic time slices:", times)
+        sys.exit(1)
+
+def testsol(sol, corrs, times=None):
+    """Test ratio in effective mass equation to see if it's less < 0."""
+    if not test_arg(sol, SENT):
+        for i, corr in enumerate(corrs):
+            print("corrs["+str(i)+"] = ", corr)
+        if times is not None:
+            print("problematic time slices:", times)
+        sys.exit(1)
