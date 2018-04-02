Correct boosted zeta calculation.

latfit/utilities/zeta/zeta.py
@@ -5,6 +5,7 @@ import inspect
 import os
 import math
 import numpy as np
+from math import sqrt
 from matplotlib.backends.backend_pdf import PdfPages
 from latfit.config import PION_MASS, L_BOX, CALC_PHASE_SHIFT, START_PARAMS, PTOTSQ, AINVERSE, ISOSPIN, MOMSTR
 from latfit.utilities import read_file as rf
@@ -26,13 +27,14 @@ if CALC_PHASE_SHIFT:
                 epipi = epipi[0]
             except (IndexError, TypeError):
                 pass
-        epipi = epipi*AINVERSE
+        comp = np.array(rf.procmom(MOMSTR))
+        gamma = epipi/sqrt(epipi**2-(2*np.pi/L_BOX)**2*np.dot(comp, comp))
+        epipi = epipi*AINVERSE/gamma
 
-        comp = np.array(rf.procmom(MOMSTR))*AINVERSE
         #epipi = math.sqrt(epipi**2-(2*np.pi/L_BOX)**2*PTOTSQ) //not correct
         binpath = os.path.dirname(inspect.getfile(zeta))+'/main.o'
         arglist = [binpath, str(epipi), str(PION_MASS), str(L_BOX),
-                   str(comp[0]), str(comp[1]), str(comp[2])]
+                   str(comp[0]), str(comp[1]), str(comp[2]), str(gamma)]
         try:
             out = subprocess.check_output(arglist)
         except FileNotFoundError: