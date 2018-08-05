goracle/lattice-fitter

commit cda4d7fafff98663abc486317b52daf9beb60052

Add debugging tools (XMAX doesn't work yet.)

latfit/extract/getblock.py
@@ -10,6 +10,7 @@ from latfit.mathfun.proc_meff import proc_meff
 from latfit.mathfun.elim_jkconfigs import elim_jkconfigs
 from latfit.mathfun.binconf import binconf
 from latfit.extract.proc_line import proc_line
+from latfit.jackknife_fit import jack_mean_err
 
 from latfit.config import EFF_MASS
 from latfit.config import GEVP
@@ -22,6 +23,8 @@ from latfit.config import MATRIX_SUBTRACTION
 
 if MATRIX_SUBTRACTION and GEVP:
     ADD_CONST_VEC = [0 for i in ADD_CONST_VEC]
+
+XMAX = 999
 
 if PIONRATIO:
     PIONSTR = ['pioncorrChk_mom'+str(i)+'unit'+('s' if i != 1 else '') for i in range(2)]
@@ -74,6 +77,8 @@ def get_eigvals(num, file_tup_lhs, file_tup_rhs, overb=False, print_evecs=False)
     c_rhs = np.zeros((dimops, dimops), dtype=float)
     for opa in range(dimops):
         for opb in range(dimops):
+            if opb != opa:
+                continue
             c_lhs[opa][opb] = proc_line(
                 getline_loc(file_tup_lhs[opa][opb], num+1),
                 file_tup_lhs[opa][opb])*NORMS[opa][opb]
@@ -137,6 +142,7 @@ if EFF_MASS:
             num_configs = len(file_tup[0][0][0])
         if GEVP_DEBUG:
             print("Getting block for time slice=", timeij)
+        check_variance = []
         for num in range(num_configs):
             if GEVP_DEBUG:
                 print("config #=", num)
@@ -151,7 +157,8 @@ if EFF_MASS:
                                               overb=True), reverse=True)
 
                 if PIONRATIO:
-                    div = np.array([np.real(PION[i][num][int(timeij)]**2-PION[i][num][int(
+                    div = np.array([np.real(
+                        PION[i][num][int(timeij)]**2-PION[i][num][int(
                         timeij)+1]**2) for i in range(dimops)])/1e10
                     eigvals /= div
                     eigvals2 /= div
@@ -164,10 +171,16 @@ if EFF_MASS:
                 #print(num, file_tup)
                 print('config_num:', num, 'time:', timeij)
                 raise XmaxError(problemx=timeij)
+            check_variance.append(eigvals)
             retblk.append(np.array([proc_meff(
                 (eigvals[op], eigvals2[op], eigvals3[op],
                  eigvals4[op]), index=op, time_arr=timeij)
                                     for op in range(dimops)]))
+        if GEVP_DEBUG:
+            print("time, avg evals, variance of evals:",
+                  timeij, jack_mean_err(np.array(check_variance)))
+            if timeij == 10:
+                sys.exit(0)
         return retblk
 
 else:

latfit/__main__.py
@@ -36,6 +36,7 @@ from latfit.config import MATRIX_SUBTRACTION, DELTA_T_MATRIX_SUBTRACTION
 from latfit.config import GEVP, FIT, STYPE
 from latfit.config import MAX_ITER, FITSTOP, BIASED_SPEEDUP
 from latfit.jackknife_fit import ResultMin, jack_mean_err
+import latfit.extract.getblock
 
 from latfit.procargs import procargs
 from latfit.extract.errcheck.xlim_err import xlim_err
@@ -81,7 +82,7 @@ sys.stderr = Logger()
 
 def xmin_mat_sub(xmin, xstep=1):
     ret = xmin
-    if GEVP:
+    if GEVP and MATRIX_SUBTRACTION:
         if xmin < DELTA_T_MATRIX_SUBTRACTION:
             ret = DELTA_T_MATRIX_SUBTRACTION + xstep
     return ret
@@ -144,6 +145,7 @@ def main():
     # error processing, parameter extractions
     input_f, options = procargs(sys.argv[1:])
     xmin, xmax = xlim_err(options.xmin, options.xmax)
+    latfit.extract.getblock.XMAX = xmax
     xstep = xstep_err(options.xstep, input_f)
     xmin = xmin_mat_sub(xmin, xstep=xstep)
     fitrange = fitrange_err(options, xmin, xmax)
@@ -159,6 +161,7 @@ def main():
             _ = singlefit(input_f, fitrange, xmin, xmax, xstep)
         except XmaxError as err:
             xmax = err.problemx-xstep
+            latfit.extract.getblock.XMAX = xmax
             print("xmin, new xmax =", xmin, xmax)
             if fitrange[1] > xmax and FIT:
                 print("***ERROR***")

latfit/config.py
@@ -56,6 +56,7 @@ IRREP = 'T_1_MINUS'
 IRREP = 'T_1_3MINUS'
 IRREP = 'T_1_MINUS'
 IRREP = 'A_1PLUS_mom000'
+IRREP = 'A1x_mom011'
 IRREP = 'A1_mom11'
 # non-zero center of mass
 MOMSTR = opc.get_comp_str(IRREP)
@@ -94,13 +95,20 @@ TSEP_VEC = [3, 0, 3]
 TSEP_VEC = [3]*DIM if GEVP else [0]
 LT = 64
 
+# print raw gevp info (for debugging source construction)
+
+GEVP_DEBUG = False
+GEVP_DEBUG = True
+
 # additive constant, due to around-the-world effect
 # do the subtraction at the level of the GEVP matrix
 MATRIX_SUBTRACTION = False
 MATRIX_SUBTRACTION = True
-DELTA_T_MATRIX_SUBTRACTION = 3
+MATRIX_SUBTRACTION = False if GEVP_DEBUG else MATRIX_SUBTRACTION
+DELTA_T_MATRIX_SUBTRACTION = 3 if not GEVP_DEBUG else 0
 # do the subtraction at the level of the eigenvalues
 ADD_CONST_VEC = [True]*DIM if GEVP else [False]
+ADD_CONST_VEC = [False]*DIM if GEVP_DEBUG else ADD_CONST_VEC
 ADD_CONST = ADD_CONST_VEC[0] or (MATRIX_SUBTRACTION and GEVP)  # no need to modify
 
 # exclude from fit range these time slices.  shape = (GEVP dim, tslice elim)
@@ -121,11 +129,6 @@ ELIM_JKCONF_LIST = []
 # dynamic binning of configs.  BINNUM is number of configs per bin.
 BINNUM = 1
 
-# print raw gevp info (for debugging source construction)
-
-GEVP_DEBUG = True
-GEVP_DEBUG = False
-
 # stringent tolerance for minimizer?  true = stringent
 MINTOL = True
 MINTOL = False
@@ -138,7 +141,7 @@ RESCALE = 1.0
 
 T0 = 'ROUND' # ceil(t/2)
 T0 = 'TMINUS1' # t-1
-T0 = 'TMINUS1' if ISOSPIN == 2 else 'ROUND'
+T0 = 'TMINUS1' if ISOSPIN == 0 else 'ROUND'
 
 # Pion ratio?  Put single pion correlators in the denominator
 # of the eff mass equation to get better statistics.
@@ -383,7 +386,11 @@ LT_VEC = []
 for tsep in TSEP_VEC:
     LT_VEC.append(LT-2*tsep)
 LT = LT_VEC[0]
-assert MATRIX_SUBTRACTION or not GEVP, "Must subtract around the world constant at GEVP level"
+if not GEVP_DEBUG:
+    assert MATRIX_SUBTRACTION or not GEVP,\
+        "Must subtract around the world constant at GEVP level"
+else:
+    MATRIX_SUBTRACTION = False
 MATRIX_SUBTRACTION = False if not GEVP else MATRIX_SUBTRACTION
 if MATRIX_SUBTRACTION:
     for i, _ in enumerate(ADD_CONST_VEC):
@@ -399,7 +406,7 @@ UP.add_const = ADD_CONST
 UP.log = LOG
 UP.c = C
 # make global tstep equal to delta t so fit functions below will be setup correctly
-UP.tstep = TSTEP if not GEVP else DELTA_T_MATRIX_SUBTRACTION
+UP.tstep = TSTEP if not GEVP or GEVP_DEBUG else DELTA_T_MATRIX_SUBTRACTION
 UP.pionmass = misc.MASS
 UP.pionratio = PIONRATIO
 UP.lent = LT

commit e0531a6d1d211645f97eac76ffedd1b53c61c4c6

Fix bug introduced in cda4d7fafff98

latfit/extract/getblock.py
@@ -77,8 +77,6 @@ def get_eigvals(num, file_tup_lhs, file_tup_rhs, overb=False, print_evecs=False)
     c_rhs = np.zeros((dimops, dimops), dtype=float)
     for opa in range(dimops):
         for opb in range(dimops):
-            if opb != opa:
-                continue
             c_lhs[opa][opb] = proc_line(
                 getline_loc(file_tup_lhs[opa][opb], num+1),
                 file_tup_lhs[opa][opb])*NORMS[opa][opb]