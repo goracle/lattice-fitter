Fix kaon bugs; analysis code now ready for numerical tests.

latfit/utilities/kaonanalysis/kaonanalysis.py
@@ -159,10 +159,9 @@ def get_kdiscon_fromfile(diags, trajl):
 
 def jackknifeOPS():
     """Jackknife operators."""
-    OPS = [kpp.QOPI0, kpp.QOPI2, kpp.QOP_sigma]
     for i in np.arange(1, 11): # loop over operators
         keylist = []
-        for key in kpp.QOPI0:
+        for key in kpp.QOPI0[str(i)]:
             kpp.QOPI0[str(i)][key] = h5jack.dojackknife(
                 kpp.QOPI0[str(i)][key])
             kpp.QOPI2[str(i)][key] = h5jack.dojackknife(

latfit/utilities/kaonanalysis/kaonpostproc.py
@@ -1,4 +1,5 @@
 """Storage for post processed operators.  After these are filled we write to disk"""
+import os
 import numpy as np
 import h5py
 
@@ -14,15 +15,20 @@ for i in np.arange(1, 11):
 
 def writeOut():
     """Write the result to file"""
-    keyarr = []
+    keyarr = set()
+    if not os.path.isdir('I0'):
+        os.makedirs('I0')
+    if not os.path.isdir('I2'):
+        os.makedirs('I2')
     for i in np.arange(1, 11):
         for keyirr in QOPI0[str(i)]:
             momrel, kpitsep = keyirr.split('@')
-            keyarr.append(keyirr)
+            keyarr.add(keyirr)
             assert keyirr in QOPI0[str(i)], "tsep = "+kpitsep+\
                 " not in Q_"+str(i)+", I=0"
             assert keyirr in QOPI2[str(i)], "tsep = "+kpitsep+\
                 " not in Q_"+str(i)+", I=2"
+    keyarr = sorted(list(keyarr))
     for key in keyarr:
         momrel, kpitsep = key.split('@')
         momrel = int(momrel)
@@ -39,15 +45,27 @@ def writeOut():
             raise
         for i in np.arange(1, 11):
             filestr = 'Q'+str(i)+'_deltat_'+str(kpitsep)
-            filestr = filestr+'_'+opstr
-            filestr_sigma = filestr+'_sigma'
-            fn1 = h5py.File('I0/'+filestr, 'w')
-            fn1[filestr] = QOPI0[str(i)][key]
-            fn1.close()
-            fn1 = h5py.File('I2/'+filestr, 'w')
-            fn1[filestr] = QOPI2[str(i)][key]
-            fn1.close()
-            if momrel == 0:
-                fn1 = h5py.File('I0/'+filestr_sigma, 'w')
-                fn1[filestr] = QOP_sigma[str(i)][key]
+            filestr_pipi = filestr+'_'+opstr
+            if not os.path.isfile("I0/"+filestr_pipi):
+                print("writing file: ", "I0/"+filestr_pipi)
+                fn1 = h5py.File('I0/'+filestr_pipi, 'w')
+                fn1[filestr_pipi] = QOPI0[str(i)][key]
                 fn1.close()
+            else:
+                print("skipping extant file: ", "I0/"+filestr_pipi)
+            if not os.path.isfile("I2/"+filestr_pipi):
+                print("writing file: ", "I2/"+filestr_pipi)
+                fn1 = h5py.File('I2/'+filestr_pipi, 'w')
+                fn1[filestr_pipi] = QOPI2[str(i)][key]
+                fn1.close()
+            else:
+                print("skipping extant file: ", "I2/"+filestr_pipi)
+            if momrel == 0:
+                filestr_sigma = filestr+'_sigma'
+                if not os.path.isfile("I0/"+filestr_sigma):
+                    print("writing file: ", "I0/"+filestr_sigma)
+                    gn1 = h5py.File('I0/'+filestr_sigma, 'w')
+                    gn1[filestr_sigma] = QOP_sigma[str(i)][key]
+                    gn1.close()
+                else:
+                    print("skipping extant file: ", "I0/"+filestr_sigma)