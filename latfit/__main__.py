Fix I=1 related bugs; improve lookahead fit range avg.

latfit/__main__.py
@@ -240,37 +240,47 @@ def main():
                 except (NegChisq, RelGammaError, OverflowError, NoConvergence,
                         BadJackknifeDist, DOFNonPos,
                         BadChisqJackknife, ZetaError) as _:
-                    print("Test fit failed, but in an acceptable way. Continuing.")
+                    print("Test fit failed,"+\
+                          " but in an acceptable way. Continuing.")
                     fit_range_init = None
             cut_on_errsize()
             augment_excl.excl_orig = np.copy(latfit.config.FIT_EXCL)
-            plotdata.coords, plotdata.cov = singlefit.coords_full, singlefit.cov_full
+            plotdata.coords, plotdata.cov = singlefit.coords_full,\
+                singlefit.cov_full
             tsorted = []
             for i in range(MULT):
                 #if MULT == 1:
                 #    break
                 if i == 0 and MPIRANK == 0:
-                    print("Finding best times (most likely to give small chi^2 contributions)")
+                    print("Finding best times ("+\
+                          "most likely to give small chi^2 contributions)")
                 if MULT > 1:
                     coords = np.array([j[i] for j in plotdata.coords[:,1]])
                 else:
                     coords = np.array([j for j in plotdata.coords[:,1]])
                 times = np.array(list(plotdata.coords[:,0]))
                 if MULT > 1:
-                    tsorted.append(sortfit.best_times(coords, plotdata.cov[:,:,i,i], i, times))
+                    tsorted.append(sortfit.best_times(
+                        coords, plotdata.cov[:,:,i,i], i, times))
                 else:
-                    tsorted.append(sortfit.best_times(coords, plotdata.cov, 0, times))
+                    tsorted.append(
+                        sortfit.best_times(coords, plotdata.cov, 0, times))
             samp_mult = []
             if random_fit:
-                # go in a random order if lenprod is small (biased by how likely fit will succeed),
+                # go in a random order if lenprod is small
+                # (biased by how likely fit will succeed),
                 for i in range(MULT):
                     #if MULT == 1:
                     #    break
                     if i == 0 and MPIRANK == 0 and BIASED_SPEEDUP:
-                        print("Setting up biased sorting of (random) fit ranges")
-                    probs, sampi = sortfit.sample_norms(
-                        sampler, tsorted[i], lenfit)
-                    probs = probs if BIASED_SPEEDUP else None
+                        print("Setting up biased sorting of"+\
+                              " (random) fit ranges")
+                    if BIASED_SPEEDUP:
+                        probs, sampi = sortfit.sample_norms(
+                            sampler, tsorted[i], lenfit)
+                    else:
+                        probs =  None
+                        sampi = sorted(list(sampler))
                     samp_mult.append([probs, sampi])
             else:
                 for i in range(MULT):
@@ -404,8 +414,11 @@ def main():
                 # is this justifiable?
                 if CALC_PHASE_SHIFT and MULT > 1:
                     if any(result_min.phase_shift_err > PHASE_SHIFT_ERR_CUT):
-                        print("phase shift errors too large")
-                        continue
+                        if all(result_min.phase_shift_err[:-1] < PHASE_SHIFT_ERR_CUT):
+                            print("warning: phase shift errors on last state very large")
+                        else:
+                            print("phase shift errors too large")
+                            continue
 
 
                 # calculate average relative error (to be minimized)
@@ -421,17 +434,18 @@ def main():
                     overfit_arr.append(result)
                     continue
 
-                errarr.append(param_err)
-                curr_err, avg_curr_err = errerr(errarr)
-                print("average statistical error on parameters",
-                      avg_curr_err)
-                stop = max(curr_err)/avg_curr_err[np.argmax(curr_err)]
-                if stop < FITSTOP:
-                    print("Estimate for parameter error has"+\
-                          " stabilized, exiting loop")
-                    break
-                else:
-                    print("Current error on error =", curr_err)
+                # obsolete
+                #errarr.append(param_err)
+                #curr_err, avg_curr_err = errerr(errarr)
+                #print("average statistical error on parameters",
+                #      avg_curr_err)
+                #stop = max(curr_err)/avg_curr_err[np.argmax(curr_err)]
+                #if stop < FITSTOP:
+                #    print("Estimate for parameter error has"+\
+                #          " stabilized, exiting loop")
+                #    break
+                #else:
+                #    print("Current error on error =", curr_err)
 
                 # need better criterion here,
                 # maybe just have it be user defined patience level?

latfit/analysis/sortfit.py
@@ -61,15 +61,17 @@ def sample_norms(sampler, tsorted, lenfit):
     samp = sorted(list(sampler))
     total = 0
     probs = []
+    assert len(samp) != 0, "no fit ranges"
     for excl in samp:
         score = score_excl(excl, tsorted, lenfit, inversescore=False)
         probs.append(score)
         total += score
     assert not np.isnan(total), "Score total is not a number."
+    assert total != 0, "total is 0."
     norm = 1.0/total
     probs = np.array(probs)*norm
     assert np.allclose(
-        np.sum(probs, axis=0), 1.0), "Probabilities chosen do not sum to 1."
+        np.sum(probs, axis=0), 1.0, rtol=1e-8), "Probabilities chosen do not sum to 1:"+str(np.sum(probs, axis=0))
     #for i, j in zip(samp, probs):
     #    print(i, j)
     #sys.exit(0)

latfit/config.py
@@ -52,7 +52,7 @@ SUPERJACK_CUTOFF = 0
 SUPERJACK_CUTOFF = 7
 
 # isospin value, (0,1,2 supported)
-ISOSPIN = 2
+ISOSPIN = 1
 
 # group irrep
 IRREP = 'T_1_2MINUS'
@@ -62,8 +62,8 @@ IRREP = 'A1x_mom011'
 IRREP = 'A1_avg_mom111'
 IRREP = 'A1_avg_mom111'
 IRREP = 'A_1PLUS_mom000'
-IRREP = 'T_1_MINUS'
 IRREP = 'A1_mom1'
+IRREP = 'T_1_MINUS'
 # non-zero center of mass
 MOMSTR = opc.get_comp_str(IRREP)
 
@@ -91,6 +91,7 @@ DISP_ENERGIES = opc.free_energies(IRREP, misc.MASS, L_BOX) if GEVP else []
 # don't include the sigma in the gevp fits
 SIGMA = True if ISOSPIN == 0 else False
 DIM = len(DISP_ENERGIES) + (1 if SIGMA or ISOSPIN == 1 else 0) # no need to change
+#DIM = 4
 DISP_ENERGIES = list(np.array(DISP_ENERGIES)[:DIM])
 
 # time extent (1/2 is time slice where the mirroring occurs in periodic bc's)
@@ -109,8 +110,8 @@ GEVP_DEBUG = False
 
 # additive constant, due to around-the-world effect
 # do the subtraction at the level of the GEVP matrix
-MATRIX_SUBTRACTION = False
 MATRIX_SUBTRACTION = True
+MATRIX_SUBTRACTION = False
 MATRIX_SUBTRACTION = False if GEVP_DEBUG else MATRIX_SUBTRACTION
 MATRIX_SUBTRACTION = False if ISOSPIN == 1 else MATRIX_SUBTRACTION
 MATRIX_SUBTRACTION = False if not GEVP else MATRIX_SUBTRACTION
@@ -177,8 +178,8 @@ RESCALE = 1.0
 # T0 behavior for GEVP (t/2 or t-1)
 
 T0 = 'TMINUS1' if ISOSPIN == 2 else 'ROUND'
-T0 = 'ROUND' # ceil(t/2)
 T0 = 'TMINUS1' # t-1
+T0 = 'ROUND' # ceil(t/2)
 
 # Pion ratio?  Put single pion correlators in the denominator
 # of the eff mass equation to get better statistics.
@@ -248,7 +249,7 @@ if GEVP:
             r' GEVP, I2, $\pi\pi$, ' + PSTR_TITLE + ' '
     elif ISOSPIN == 1:
         TITLE_PREFIX = str(DIM)+r'x'+str(DIM)+\
-            r' GEVP, I1, $\pi\pi,~\rho$ ' + PSTR_TITLE + ' '
+            r' GEVP, I1, $\pi\pi, \rho$ ' + PSTR_TITLE + ' '
     else:
         TITLE_PREFIX = str(DIM)+r'x'+str(DIM)+\
             r' GEVP, $\pi\pi$, ' + PSTR_TITLE + ' '
@@ -298,7 +299,7 @@ CALC_PHASE_SHIFT = False if not GEVP else CALC_PHASE_SHIFT
 
 # phase shift error cut, absolute, in degrees.
 # if the error is bigger than this, skip this fit range
-PHASE_SHIFT_ERR_CUT = 20 if ISOSPIN == 2 else np.inf
+PHASE_SHIFT_ERR_CUT = 20 if ISOSPIN != 0 else np.inf
 
 # skip fit range if parameter (energy) errors greater than 100%
 SKIP_LARGE_ERRORS = False
@@ -314,7 +315,7 @@ PLOT_DISPERSIVE = False if not GEVP else True
 
 # Decrease variance in GEVP (avoid eigenvalue misordering due to large noise)
 # should be < 1
-DECREASE_VAR = 1e-3
+DECREASE_VAR = 1e-6
 
 # precision to display, number of decimal places
 
@@ -394,7 +395,8 @@ NORMS = [[1.0/10**6, 1.0/10**3/10**(2.5),
           1.0/10**2.5/10**5.5],
          [1.0/10**3/10**5.5,
           1.0/10**2.5/10**5.5, 1.0/10**11]]
-NORMS = [[1.0 for _ in range(DIM)] for _ in range(DIM)]
+NORMS = [[(-1 if i ==1 else 1)*(-1 if j==1 else 1) for i in range(DIM)] for j in range(DIM)]
+NORMS = [[1 for i in range(DIM)] for j in range(DIM)]
 
 # GENERALIZED PENCIL OF FUNCTION (see arXiv:1010.0202, for use with GEVP)
 # if non-zero, set to 1 (only do one pencil,

latfit/extract/getblock.py
@@ -85,11 +85,8 @@ def readin_gevp_matrices(file_tup, num_configs, decrease_var=DECREASE_VAR):
         checkherm(cmat[num])
     mean = np.mean(cmat, axis=0)
     checkherm(mean)
-    if decrease_var:
-        check = np.array(cmat)
-        cmat = (cmat-mean)*decrease_var+mean
-        check2 = (cmat-mean)/decrease_var+mean
-        assert np.allclose(check, check2, rtol=1e-10), "precision loss detected." 
+    if decrease_var != 0:
+        cmat = variance_reduction(cmat, mean)
     return np.asarray(cmat), np.asarray(mean)
 
 def checkherm(carr):
@@ -119,7 +116,7 @@ def get_eigvals(c_lhs, c_rhs, overb=False, print_evecs=False, commnorm=False):
         c_rhs_inv = linalg.inv(c_rhs)
         # compute commutator divided by norm to see how close rhs and lhs bases
         try:
-            commutator_norm = np.linalg.norm((np.dot(c_rhs_inv, c_lhs)-np.dot(c_lhs, c_rhs_inv))/np.linalg.norm(c_rhs_inv)/np.linalg.norm(c_lhs))
+            commutator_norms = np.linalg.norm((np.dot(c_rhs_inv, c_lhs)-np.dot(c_lhs, c_rhs_inv))/np.linalg.norm(c_rhs_inv)/np.linalg.norm(c_lhs))
         except FloatingPointError:
             print("bad denominator:")
             print(np.linalg.norm(c_rhs_inv))
@@ -146,7 +143,7 @@ def get_eigvals(c_lhs, c_rhs, overb=False, print_evecs=False, commnorm=False):
         skip_late = True
     eigfin = np.zeros((len(eigvals)), dtype=np.float)
     for i, j in enumerate(eigvals):
-        if j.imag == 0:
+        if j.imag == 0 and j.real > 0:
             eigfin[i] = eigvals[i].real
         else:
             if USE_LATE_TIMES and not skip_late and late:
@@ -203,9 +200,28 @@ def variance_reduction(orig, avg, decrease_var=DECREASE_VAR):
     """
     apply y->(y_i-<y>)*decrease_var+<y>
     """
+    nanindices = []
+    if hasattr(orig, '__iter__'):
+        assert hasattr(avg, '__iter__'), "dimension mismatch"
+        if len(orig.shape) == 1:
+            for i,j in enumerate(zip(orig, avg)):
+                    if any(np.isnan(j)):
+                        orig[i] = np.nan
+                        avg[i] = np.nan
+    else:
+        assert not np.isnan(orig+avg), "nan found"
     ret = (orig-avg)*decrease_var+avg
     check = (ret-avg)/decrease_var+avg
-    assert np.allclose(check, orig, rtol=1e-10), "precision loss detected." 
+    try:
+        assert np.allclose(check, orig, rtol=1e-8, equal_nan=True),\
+            "precision loss detected:"+str(orig)+" "+str(check)+" "+str(decrease_var)
+    except AssertionError:
+        print("precision loss detected: (check, orig, avg, ret):")
+        print(check)
+        print(orig)
+        print(avg)
+        print(ret)
+        sys.exit(1)
     return ret
 
 
@@ -243,6 +259,11 @@ if EFF_MASS:
         eigvals_mean_tp3 = sorted(get_eigvals(
             cmat_lhs_tp3_mean, cmat_rhs_mean), reverse=True)
 
+        avg_energies = np.array([proc_meff(
+            (eigvals_mean_t[op], eigvals_mean_tp1[op], eigvals_mean_tp2[op],
+             eigvals_mean_tp3[op]), index=op, time_arr=timeij)
+                         for op in range(dimops)])
+
         norm_comm = []
         norms_comm = []
         for num in range(num_configs):
@@ -253,19 +274,19 @@ if EFF_MASS:
                 eigret = get_eigvals(cmat_lhs_t[num], cmat_rhs[num], print_evecs=True, commnorm=True)
                 norm_comm.append(eigret[1])
                 norms_comm.append(eigret[2])
-                eigvals = variance_reduction(np.array(sorted(eigret[0], reverse=True)), eigvals_mean_t, 1/decrease_var)
+                eigvals = np.array(sorted(eigret[0], reverse=True))
 
                 tprob = None if not EFF_MASS else tprob
 
-                eigvals2 = variance_reduction(np.array(sorted(get_eigvals(
-                    cmat_lhs_tp1[num], cmat_rhs[num]), reverse=True)), eigvals_mean_tp1, 1/decrease_var)
+                eigvals2 = np.array(sorted(get_eigvals(
+                    cmat_lhs_tp1[num], cmat_rhs[num]), reverse=True))
 
-                eigvals3 = variance_reduction(np.array(sorted(get_eigvals(
-                    cmat_lhs_tp2[num], cmat_rhs[num]), reverse=True)), eigvals_mean_tp2, 1/decrease_var)
+                eigvals3 = np.array(sorted(get_eigvals(
+                    cmat_lhs_tp2[num], cmat_rhs[num]), reverse=True))
 
-                eigvals4 = variance_reduction(np.array(sorted(get_eigvals(
+                eigvals4 = np.array(sorted(get_eigvals(
                     cmat_lhs_tp3[num], cmat_rhs[num],
-                    overb=True), reverse=True)), eigvals_mean_tp3, 1/decrease_var)
+                    overb=True), reverse=True))
 
                 if PIONRATIO:
                     div = np.array([np.real(
@@ -284,10 +305,10 @@ if EFF_MASS:
                 if tprob is not None:
                     raise XmaxError(problemx=tprob)
             check_variance.append(eigvals)
-            retblk.append(np.array([proc_meff(
-                (eigvals[op], eigvals2[op], eigvals3[op],
-                 eigvals4[op]), index=op, time_arr=timeij)
-                                    for op in range(dimops)]))
+            result = variance_reduction(np.array([proc_meff(
+                (eigvals[op], eigvals2[op], eigvals3[op], eigvals4[op]),
+                index=op, time_arr=timeij) for op in range(dimops)]), avg_energies, 1/decrease_var)
+            retblk.append(result)
         if MPIRANK == 0:
             pass
             #chisq_bad, pval, dof = pval_commutator(norms_comm)

latfit/jackknife_fit.py
@@ -152,6 +152,9 @@ elif JACKKNIFE_FIT == 'DOUBLE' or JACKKNIFE_FIT == 'SINGLE':
         # as it is 5 \sigma away from an acceptable chi^2/dof
         chisq_fiduc_cut = chisqfiduc(params.num_configs,
                                      result_min.dof)
+        # similarly, cut below 5 sigma below chisq/dof = 1
+        chisq_fiduc_overfit_cut = overfit_chisq_fiduc(
+            params.num_configs, result_min.dof)
 
         skip_votes = []
         # loop over configs, doing a fit for each one
@@ -211,9 +214,7 @@ elif JACKKNIFE_FIT == 'DOUBLE' or JACKKNIFE_FIT == 'SINGLE':
 
                 # check if chisq too big, too small
                 if result_min_jack.fun > chisq_fiduc_cut or\
-                   result_min_jack.fun/result_min.dof < 1-5*(
-                       2 if result_min.dof < 10 else 1)*np.sqrt(2*result_min.dof)/(
-                       params.num_configs-SUPERJACK_CUTOFF):
+                   result_min_jack.fun < chisq_fiduc_overfit_cut:
                     skip_votes.append(config_num)
 
                 if config_num == 1+SUPERJACK_CUTOFF:
@@ -246,7 +247,7 @@ elif JACKKNIFE_FIT == 'DOUBLE' or JACKKNIFE_FIT == 'SINGLE':
                               str(result_min.pvalue[0])+" "+\
                               str(result_min.pvalue[1])+" ")
                         sys.exit(1)
-                        raise BadJackknifeDist
+                        #raise BadJackknifeDist
 
         # average results, compute jackknife uncertainties
 
@@ -291,19 +292,40 @@ else:
     print("Bad jackknife_fit value specified.")
     sys.exit(1)
 
+def overfit_chisq_fiduc(num_configs, dof, guess=None):
+    """Find the overfit 5 sigma cut
+    (see chisqfiduc for the lower cut on the upper bound)
+    """
+    cut = stats.chi2.cdf(dof, dof)
+    lbound = 3e-7
+    func = lambda tau: ((1-cut*lbound)-(1-stats.chi2.cdf(abs(tau), dof)))**2
+    sol = abs(float(fsolve(func, 1e-5 if guess is None else guess)))
+    sol2 = dof
+    assert abs(func(sol)) < 1e-12, "fsolve failed:"+str(num_configs)+\
+        " "+str(dof)
+    diff = (sol2-sol)/(num_configs-SUPERJACK_CUTOFF-1)
+    assert diff > 0, "bad solution to p-value solve, chisq/dof solution > 1"
+    ret = sol2-diff
+    return ret
+
 def chisqfiduc(num_configs, dof):
     """Find the chi^2/dof cutoff (acceptance upper bound)
     defined as > 5 \sigma away from an acceptable pvalue
     2*dof is the variance in chi^2
     """
-    func = lambda tau: PVALUE_MIN-(1-stats.chi2.cdf(tau, dof))
+    func = lambda tau: PVALUE_MIN*3e-7-(1-stats.chi2.cdf(tau, dof))
+    func2 = lambda tau: PVALUE_MIN-(1-stats.chi2.cdf(tau, dof))
     # guess about 2 for the max chi^2/dof
     sol = float(fsolve(func, dof))
+    sol2 = float(fsolve(func2, dof))
     assert abs(func(sol)) < 1e-8, "fsolve failed."
+    assert abs(func2(sol2)) < 1e-8, "fsolve2 failed."
     # known variance of chi^2 is 2*dof, but skewed at low dof (here chosen to be < 10)
     # thus, the notion of a "5 sigma fluctuation" is only defined as dof->inf
     # so we have a factor of 2 to make low dof p-value cut less aggressive
-    ret = sol+5*(2 if dof < 10 else 1)*np.sqrt(2*dof)/(num_configs-SUPERJACK_CUTOFF)
+    #ret = sol+5*(2 if dof < 10 else 1)*np.sqrt(2*dof)/(num_configs-SUPERJACK_CUTOFF)
+    diff = (sol-sol2)/(num_configs-SUPERJACK_CUTOFF-1)
+    ret = sol2+diff
     #print(ret/dof, sol/dof, num_configs, dof, PVALUE_MIN,
     #      1-stats.chi2.cdf(ret, dof), 1-stats.chi2.cdf(sol, dof))
     return ret