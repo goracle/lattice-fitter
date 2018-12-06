33e5c3c554598104af52958d4ee72f330a0d7260
Dan H <goracle@users.noreply.github.com>
2018-12-06 17:20:08 -0500

    Turn on fit disp. correction for 32c (then set it to 0) (2/?)

--- latfit/config.py
+++ latfit/config.py
@@ -117,6 +117,7 @@ GEVP_DEBUG = False
 # continuum dispersion relation corrected using fits (true) or phat (false)
 FIT_SPACING_CORRECTION = False
 FIT_SPACING_CORRECTION = True
+FIT_SPACING_CORRECTION = True if L_BOX == 32 else FIT_SPACING_CORRECTION
 FIT_SPACING_CORRECTION = False if ISOSPIN != 2 else FIT_SPACING_CORRECTION
 misc.CONTINUUM = FIT_SPACING_CORRECTION
 
