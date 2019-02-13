3b0dd4a19d54c95eefe243cc88dae02761d46ef0
Dan H <goracle@users.noreply.github.com>
2019-02-12 22:49:18 -0500

    Clean up; add documenation.

--- latfit/mathfun/elim_jkconfigs.py
+++ latfit/mathfun/elim_jkconfigs.py
@@ -29,10 +29,34 @@ def elim_jkconfigs(jkblk, elim_list=None):
     if k_elim == 0:
         new_jkblk = jkblk
     else:
+
+        # each of the bad configs appears k_elim-1 times in skip_sum
+        # each of the good configs appears k_elim times in skip_sum
         skip_sum = np.sum([jkblk[skip]
                            for skip in elim_list], axis=0)
-        sum_blk = np.sum(jkblk, axis=0)
-        new_jkblk = 1.0/(num_configs-1-k_elim)*((
-            num_configs-1)*(np.delete(
-                jkblk, elim_list, axis=0)+skip_sum)-k_elim*sum_blk)
+
+        sum_blk = np.sum(
+            jkblk, axis=0) # same as sum over original set, no norm
+
+        # delete the blocks corresponding to the bad configs
+        inner = np.delete(jkblk, elim_list, axis=0)+skip_sum
+
+        # check for tolerance violation
+        assert np.allclose(inner - skip_sum,
+                           np.delete(jkblk, elim_list, axis=0), rtol=1e-12)
+
+        # unormalize; only sums over configs now
+        inner *= (num_configs-1)
+        
+        # subtract the good configs we added in skip_sum
+        # subtract the bad configs we added in skip_sum, plus one extra
+        # this extra being the copy which shows up in the non-deleted blocks
+        final_diff = inner-k_elim*sum_blk
+
+        # check for tolerance violation
+        assert np.allclose(final_diff + k_elim*sum_blk, inner, rtol=1e-12)
+
+        # normalize
+        new_jkblk = final_diff/(num_configs-1-k_elim)
+
     return new_jkblk
