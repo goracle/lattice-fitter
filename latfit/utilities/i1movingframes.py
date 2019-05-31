c4214ea91ffe4d171967d646b280328bdd3a89e1
Dan H <goracle@users.noreply.github.com>
2019-05-31 12:15:50 -0400

    Finish i1 moving frame irrep list.

--- latfit/utilities/i1movingframes.py
+++ latfit/utilities/i1movingframes.py
@@ -70,35 +70,127 @@ A_1PLUS_mom001 = [
     (-0.5, 'pipi', [[1, 0, 1], [-1, 0, 0]]) ,
 ]
 
-B_mom00_1 = [
-    (-0.5, 'pipi', [[-1, 0, -1], [1, 0, 0]]) ,
-    (0.5, 'pipi', [[1, 0, -1], [-1, 0, 0]]) ,
+B_0_mom00_1  = [
+(-0.5, 'UUpipi', [[-1, 1, -1], [1, -1, 0]]) ,
+(0.5, 'UUpipi', [[1, -1, -1], [-1, 1, 0]]) ,
 ]
 
-B_mom0_10 = [
-    (-0.5, 'pipi', [[-1, -1, 0], [1, 0, 0]]) ,
-    (0.5, 'pipi', [[1, -1, 0], [-1, 0, 0]]) ,
+B_0_mom_100  = [
+(-0.5, 'UUpipi', [[-1, -1, -1], [0, 1, 1]]) ,
+(0.5, 'UUpipi', [[-1, 1, 1], [0, -1, -1]]) ,
 ]
 
-B_mom_100 = [
-    (-0.5, 'pipi', [[-1, 0, -1], [0, 0, 1]]) ,
-    (0.5, 'pipi', [[-1, 0, 1], [0, 0, -1]]) ,
+B_1_mom_100  = [
+(0.5, 'UUpipi', [[-1, -1, 1], [0, 1, -1]]) ,
+(-0.5, 'UUpipi', [[-1, 1, -1], [0, -1, 1]]) ,
 ]
 
-B_mom100 = [
-    (0.5, 'pipi', [[1, 0, -1], [0, 0, 1]]) ,
-    (-0.5, 'pipi', [[1, 0, 1], [0, 0, -1]]) ,
+B_1_mom100  = [
+(0.5, 'UUpipi', [[1, -1, -1], [0, 1, 1]]) ,
+(-0.5, 'UUpipi', [[1, 1, 1], [0, -1, -1]]) ,
 ]
 
-B_mom010 = [
-    (-0.5, 'pipi', [[-1, 1, 0], [1, 0, 0]]) ,
-    (0.5, 'pipi', [[1, 1, 0], [-1, 0, 0]]) ,
+B_0_mom0_10  = [
+(-0.5, 'UUpipi', [[-1, -1, -1], [1, 0, 1]]) ,
+(0.5, 'UUpipi', [[1, -1, 1], [-1, 0, -1]]) ,
 ]
 
-B_mom001 = [
-    (-0.5, 'pipi', [[-1, 0, 1], [1, 0, 0]]) ,
-    (0.5, 'pipi', [[1, 0, 1], [-1, 0, 0]]) ,
+B_0_mom010  = [
+(-0.5, 'UUpipi', [[-1, 1, 1], [1, 0, -1]]) ,
+(0.5, 'UUpipi', [[1, 1, -1], [-1, 0, 1]]) ,
 ]
+
+B_1_mom010  = [
+(-0.5, 'UUpipi', [[-1, 1, -1], [1, 0, 1]]) ,
+(0.5, 'UUpipi', [[1, 1, 1], [-1, 0, -1]]) ,
+]
+
+B_0_mom001  = [
+(-0.5, 'UUpipi', [[-1, -1, 1], [1, 1, 0]]) ,
+(0.5, 'UUpipi', [[1, 1, 1], [-1, -1, 0]]) ,
+]
+
+B_1_mom001  = [
+(-0.5, 'UUpipi', [[-1, 1, 1], [1, -1, 0]]) ,
+(0.5, 'UUpipi', [[1, -1, 1], [-1, 1, 0]]) ,
+]
+
+B_1_mom0_10  = [
+(-0.5, 'UUpipi', [[-1, -1, 1], [1, 0, -1]]) ,
+(0.5, 'UUpipi', [[1, -1, -1], [-1, 0, 1]]) ,
+]
+
+B_0_mom100  = [
+(-0.5, 'UUpipi', [[1, -1, 1], [0, 1, -1]]) ,
+(0.5, 'UUpipi', [[1, 1, -1], [0, -1, 1]]) ,
+]
+
+B_1_mom00_1  = [
+(-0.5, 'UUpipi', [[-1, -1, -1], [1, 1, 0]]) ,
+(0.5, 'UUpipi', [[1, 1, -1], [-1, -1, 0]]) ,
+]
+
+
+B_0_mom00_1  += [
+(-0.5, 'pipi', [[-1, 0, -1], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, 0, -1], [-1, 0, 0]]) ,
+]
+
+B_0_mom_100  += [
+(-0.5, 'pipi', [[-1, 0, -1], [0, 0, 1]]) ,
+(0.5, 'pipi', [[-1, 0, 1], [0, 0, -1]]) ,
+]
+
+B_1_mom_100  += [
+(-0.5, 'pipi', [[-1, 0, -1], [0, 0, 1]]) ,
+(0.5, 'pipi', [[-1, 0, 1], [0, 0, -1]]) ,
+]
+
+B_1_mom100  += [
+(0.5, 'pipi', [[1, 0, -1], [0, 0, 1]]) ,
+(-0.5, 'pipi', [[1, 0, 1], [0, 0, -1]]) ,
+]
+
+B_0_mom0_10  += [
+(-0.5, 'pipi', [[-1, -1, 0], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, -1, 0], [-1, 0, 0]]) ,
+]
+
+B_0_mom010  += [
+(-0.5, 'pipi', [[-1, 1, 0], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, 1, 0], [-1, 0, 0]]) ,
+]
+
+B_1_mom010  += [
+(-0.5, 'pipi', [[-1, 1, 0], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, 1, 0], [-1, 0, 0]]) ,
+]
+
+B_0_mom001  += [
+(-0.5, 'pipi', [[-1, 0, 1], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, 0, 1], [-1, 0, 0]]) ,
+]
+
+B_1_mom001  += [
+(-0.5, 'pipi', [[-1, 0, 1], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, 0, 1], [-1, 0, 0]]) ,
+]
+
+B_1_mom0_10  += [
+(-0.5, 'pipi', [[-1, -1, 0], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, -1, 0], [-1, 0, 0]]) ,
+]
+
+B_0_mom100  += [
+(0.5, 'pipi', [[1, 0, -1], [0, 0, 1]]) ,
+(-0.5, 'pipi', [[1, 0, 1], [0, 0, -1]]) ,
+]
+
+B_1_mom00_1  += [
+(-0.5, 'pipi', [[-1, 0, -1], [1, 0, 0]]) ,
+(0.5, 'pipi', [[1, 0, -1], [-1, 0, 0]]) ,
+]
+
 
 ## p11 == A1^+ (12) \circleplus A2^ + \circleplus A2^-
 
@@ -699,6 +791,37 @@ A_1PLUS_mom1_11  = [
 ]
 
 
+A_1PLUS_mom111  += [
+(-1.0, 'S_pipi', [[1, 1, 1], [0, 0, 0]]) ,
+]
+
+A_1PLUS_mom_1_11  += [
+(-1.0, 'S_pipi', [[-1, -1, 1], [0, 0, 0]]) ,
+]
+
+A_1PLUS_mom11_1  += [
+(-1.0, 'S_pipi', [[1, 1, -1], [0, 0, 0]]) ,
+]
+
+A_1PLUS_mom_111  += [
+(-1.0, 'S_pipi', [[-1, 1, 1], [0, 0, 0]]) ,
+]
+
+A_1PLUS_mom_11_1  += [
+(-1.0, 'S_pipi', [[-1, 1, -1], [0, 0, 0]]) ,
+]
+
+A_1PLUS_mom_1_1_1  += [
+(-1.0, 'S_pipi', [[-1, -1, -1], [0, 0, 0]]) ,
+]
+
+A_1PLUS_mom1_11  += [
+(-1.0, 'S_pipi', [[1, -1, 1], [0, 0, 0]]) ,
+]
+
+A_1PLUS_mom1_1_1  += [
+(-1.0, 'S_pipi', [[1, -1, -1], [0, 0, 0]]) ,
+]
 
 # add an operator
 
@@ -738,102 +861,254 @@ A_1PLUS_mom1_1_1  += [
 
 # next p111 irrep, B
 
-B_mom11_1  = [
-     (1/sqrt(2), 'pipi', [[1, 0, -1], [0, 1, 0]]) ,
-     (-1/sqrt(2), 'pipi', [[1, 1, 0], [0, 0, -1]]) ,
- ]
-
-B_mom1_11  = [
-    (-1/sqrt(6), 'pipi', [[0, -1, 1], [1, 0, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[1, -1, 0], [0, 0, 1]]) ,
-    (sqrt(2/3), 'pipi', [[1, 0, 1], [0, -1, 0]]) ,
+B_1_mom11_1  = [
+(1/sqrt(2), 'pipi', [[1, 0, -1], [0, 1, 0]]) ,
+(-1/sqrt(2), 'pipi', [[1, 1, 0], [0, 0, -1]]) ,
 ]
 
-B_mom_1_1_1  = [
-    (sqrt(2/3), 'pipi', [[-1, -1, 0], [0, 0, -1]]) ,
-    (-1/sqrt(6), 'pipi', [[-1, 0, -1], [0, -1, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[0, -1, -1], [-1, 0, 0]]) ,
+B_0_mom1_11  = [
+(-1/sqrt(6), 'pipi', [[0, -1, 1], [1, 0, 0]]) ,
+(-1/sqrt(6), 'pipi', [[1, -1, 0], [0, 0, 1]]) ,
+(sqrt(2/3), 'pipi', [[1, 0, 1], [0, -1, 0]]) ,
 ]
 
-B_mom_1_1_1  = [
-    (-1/sqrt(2), 'pipi', [[-1, 0, -1], [0, -1, 0]]) ,
-    (1/sqrt(2), 'pipi', [[0, -1, -1], [-1, 0, 0]]) ,
+B_0_mom_1_1_1  = [
+(sqrt(2/3), 'pipi', [[-1, -1, 0], [0, 0, -1]]) ,
+(-1/sqrt(6), 'pipi', [[-1, 0, -1], [0, -1, 0]]) ,
+(-1/sqrt(6), 'pipi', [[0, -1, -1], [-1, 0, 0]]) ,
 ]
 
-B_mom_1_11  = [
-    (-1/sqrt(6), 'pipi', [[-1, -1, 0], [0, 0, 1]]) ,
-    (sqrt(2/3), 'pipi', [[-1, 0, 1], [0, -1, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[0, -1, 1], [-1, 0, 0]]) ,
+B_1_mom_1_1_1  = [
+(-1/sqrt(2), 'pipi', [[-1, 0, -1], [0, -1, 0]]) ,
+(1/sqrt(2), 'pipi', [[0, -1, -1], [-1, 0, 0]]) ,
 ]
 
-B_mom111  = [
-    (-1/sqrt(2), 'pipi', [[0, 1, 1], [1, 0, 0]]) ,
-    (1/sqrt(2), 'pipi', [[1, 0, 1], [0, 1, 0]]) ,
+B_0_mom_1_11  = [
+(-1/sqrt(6), 'pipi', [[-1, -1, 0], [0, 0, 1]]) ,
+(sqrt(2/3), 'pipi', [[-1, 0, 1], [0, -1, 0]]) ,
+(-1/sqrt(6), 'pipi', [[0, -1, 1], [-1, 0, 0]]) ,
 ]
 
-B_mom_1_11  = [
-    (-1/sqrt(2), 'pipi', [[-1, -1, 0], [0, 0, 1]]) ,
-    (1/sqrt(2), 'pipi', [[0, -1, 1], [-1, 0, 0]]) ,
+B_1_mom111  = [
+(-1/sqrt(2), 'pipi', [[0, 1, 1], [1, 0, 0]]) ,
+(1/sqrt(2), 'pipi', [[1, 0, 1], [0, 1, 0]]) ,
 ]
 
-B_mom111  = [
-    (-1/sqrt(6), 'pipi', [[0, 1, 1], [1, 0, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[1, 0, 1], [0, 1, 0]]) ,
-    (sqrt(2/3), 'pipi', [[1, 1, 0], [0, 0, 1]]) ,
+B_1_mom_1_11  = [
+(-1/sqrt(2), 'pipi', [[-1, -1, 0], [0, 0, 1]]) ,
+(1/sqrt(2), 'pipi', [[0, -1, 1], [-1, 0, 0]]) ,
 ]
 
-B_mom_111  = [
-    (-1/sqrt(2), 'pipi', [[-1, 0, 1], [0, 1, 0]]) ,
-    (1/sqrt(2), 'pipi', [[0, 1, 1], [-1, 0, 0]]) ,
+B_0_mom111  = [
+(-1/sqrt(6), 'pipi', [[0, 1, 1], [1, 0, 0]]) ,
+(-1/sqrt(6), 'pipi', [[1, 0, 1], [0, 1, 0]]) ,
+(sqrt(2/3), 'pipi', [[1, 1, 0], [0, 0, 1]]) ,
 ]
 
-B_mom_11_1  = [
-    (-1/sqrt(2), 'pipi', [[-1, 1, 0], [0, 0, -1]]) ,
-    (1/sqrt(2), 'pipi', [[0, 1, -1], [-1, 0, 0]]) ,
+B_1_mom_111  = [
+(-1/sqrt(2), 'pipi', [[-1, 0, 1], [0, 1, 0]]) ,
+(1/sqrt(2), 'pipi', [[0, 1, 1], [-1, 0, 0]]) ,
 ]
 
-B_mom1_1_1  = [
-    (1/sqrt(2), 'pipi', [[0, -1, -1], [1, 0, 0]]) ,
-    (-1/sqrt(2), 'pipi', [[1, -1, 0], [0, 0, -1]]) ,
+B_1_mom_11_1  = [
+(-1/sqrt(2), 'pipi', [[-1, 1, 0], [0, 0, -1]]) ,
+(1/sqrt(2), 'pipi', [[0, 1, -1], [-1, 0, 0]]) ,
 ]
 
-B_mom1_11  = [
-    (-1/sqrt(2), 'pipi', [[0, -1, 1], [1, 0, 0]]) ,
-    (1/sqrt(2), 'pipi', [[1, -1, 0], [0, 0, 1]]) ,
+B_1_mom1_1_1  = [
+(1/sqrt(2), 'pipi', [[0, -1, -1], [1, 0, 0]]) ,
+(-1/sqrt(2), 'pipi', [[1, -1, 0], [0, 0, -1]]) ,
 ]
 
-B_mom_11_1  = [
-    (sqrt(2/3), 'pipi', [[-1, 0, -1], [0, 1, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[-1, 1, 0], [0, 0, -1]]) ,
-    (-1/sqrt(6), 'pipi', [[0, 1, -1], [-1, 0, 0]]) ,
+B_1_mom1_11  = [
+(-1/sqrt(2), 'pipi', [[0, -1, 1], [1, 0, 0]]) ,
+(1/sqrt(2), 'pipi', [[1, -1, 0], [0, 0, 1]]) ,
 ]
 
-B_mom1_1_1  = [
-    (-1/sqrt(6), 'pipi', [[0, -1, -1], [1, 0, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[1, -1, 0], [0, 0, -1]]) ,
-    (sqrt(2/3), 'pipi', [[1, 0, -1], [0, -1, 0]]) ,
+B_0_mom_11_1  = [
+(sqrt(2/3), 'pipi', [[-1, 0, -1], [0, 1, 0]]) ,
+(-1/sqrt(6), 'pipi', [[-1, 1, 0], [0, 0, -1]]) ,
+(-1/sqrt(6), 'pipi', [[0, 1, -1], [-1, 0, 0]]) ,
 ]
 
-B_mom11_1  = [
-    (sqrt(2/3), 'pipi', [[0, 1, -1], [1, 0, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[1, 0, -1], [0, 1, 0]]) ,
-    (-1/sqrt(6), 'pipi', [[1, 1, 0], [0, 0, -1]]) ,
+B_0_mom1_1_1  = [
+(-1/sqrt(6), 'pipi', [[0, -1, -1], [1, 0, 0]]) ,
+(-1/sqrt(6), 'pipi', [[1, -1, 0], [0, 0, -1]]) ,
+(sqrt(2/3), 'pipi', [[1, 0, -1], [0, -1, 0]]) ,
 ]
 
-B_mom_111  = [
-    (-1/sqrt(6), 'pipi', [[-1, 0, 1], [0, 1, 0]]) ,
-    (sqrt(2/3), 'pipi', [[-1, 1, 0], [0, 0, 1]]) ,
-    (-1/sqrt(6), 'pipi', [[0, 1, 1], [-1, 0, 0]]) ,
+B_0_mom11_1  = [
+(sqrt(2/3), 'pipi', [[0, 1, -1], [1, 0, 0]]) ,
+(-1/sqrt(6), 'pipi', [[1, 0, -1], [0, 1, 0]]) ,
+(-1/sqrt(6), 'pipi', [[1, 1, 0], [0, 0, -1]]) ,
 ]
 
+B_0_mom_111  = [
+(-1/sqrt(6), 'pipi', [[-1, 0, 1], [0, 1, 0]]) ,
+(sqrt(2/3), 'pipi', [[-1, 1, 0], [0, 0, 1]]) ,
+(-1/sqrt(6), 'pipi', [[0, 1, 1], [-1, 0, 0]]) ,
+]
+
+
+def row(irr):
+    ret = irr.split('_')[1]
+    return int(ret)
+
+
+def pol_coeff(comp):
+    """Get polarization coefficients
+    from center of mass momentum"""
+    isnzero = int(sum([abs(i) for i in comp]))
+    if isnzero == 3:
+        ret = np.cross(v, [1, 0, 0])
+    elif isnzero == 2:
+        assert None, "fix this"
+        ret = []
+    else:
+        assert None, "comp should be p111 or p11"
+    return list(ret)
+
+def lstr(arr):
+    """Make sure the string will convert back to a list"""
+    ret = list(arr)
+    ret = str(ret)
+    return ret
+
+# add the rho operator via this hack
+OPLIST = {}
 for irr in dir(cmod):
     if 'mom' not in irr:
-        continue
-    if 'A_1PLUS' not in irr:
         continue
     else:
         irrvar = getattr(cmod, irr)
         mom = rf.mom(irr)
         toadd = [(1, 'rho', list(mom))]
-        irrvar += toadd
+        irrvar.insert(1, *toadd)
+        key = str(irr)+'?pol='
+        tarr = [bool(i) for i in mom] 
 
+        #pols for p1
+        if 'A_1PLUS' in irr and sum(np.abs(mom)) == 1:
+            key += str(tarr.index(True)+1)
+        elif 'B' in irr and sum(np.abs(mom)) == 1:
+            if row(irr):
+                tarr[tarr.index(False)] = True
+            pol = int(tarr.index(False)) + 1
+            key += str(pol)
+        
+        #pols for p11
+        elif 'A_1PLUS' in irr and sum(np.abs(mom)) == 2:
+            key += lstr(mom)
+        elif 'A_2PLUS' in irr and sum(np.abs(mom)) == 2:
+            key += str(tarr.index(False)+1)
+        elif 'A_1MINUS' in irr and sum(np.abs(mom)) == 2:
+            key += lstr(np.cross(mom, [(1 if not i else 0) for i in tarr]))
+
+        #pols for p111
+        elif 'A_1PLUS' in irr and sum(np.abs(mom)) == 3:
+            key += lstr(mom)
+        elif 'B' in irr and sum(np.abs(mom)) == 3:
+            pollist = pol_coeff(mom)
+            if row(irr):
+                key += lstr(np.cross(pollist, mom))
+            else:
+                key += lstr(pollist)
+
+        # default
+        else:
+            assert None, "bad irrep specified:"+str(irr)
+        OPLIST[key] = irrvar
+
+AVG_ROWS = {
+    'A_1PLUS_mom1': ('A_1PLUS_mom00_1',
+                     'A_1PLUS_mom001',
+                     'A_1PLUS_mom010',
+                     'A_1PLUS_mom0_10',
+                     'A_1PLUS_mom_100',
+                     'A_1PLUS_mom100',
+    ),
+    'B_mom1': ('B_1_mom00_1',
+               'B_1_mom001',
+               'B_1_mom010',
+               'B_1_mom0_10',
+               'B_1_mom_100',
+               'B_1_mom100',
+               'B_0_mom00_1',
+               'B_0_mom001',
+               'B_0_mom010',
+               'B_0_mom0_10',
+               'B_0_mom_100',
+               'B_0_mom100',
+    ),
+    'A_1PLUS_mom11': ('A_1PLUS_mom011',
+                      'A_1PLUS_mom0_11',
+                      'A_1PLUS_mom01_1',
+                      'A_1PLUS_mom0_1_1',
+                      'A_1PLUS_mom101',
+                      'A_1PLUS_mom_101',
+                      'A_1PLUS_mom10_1',
+                      'A_1PLUS_mom_10_1',
+                      'A_1PLUS_mom110',
+                      'A_1PLUS_mom_110',
+                      'A_1PLUS_mom1_10',
+                      'A_1PLUS_mom_1_10',
+    ),
+    'A_2PLUS_mom11': ('A_2PLUS_mom011',
+                      'A_2PLUS_mom0_11',
+                      'A_2PLUS_mom01_1',
+                      'A_2PLUS_mom0_1_1',
+                      'A_2PLUS_mom101',
+                      'A_2PLUS_mom_101',
+                      'A_2PLUS_mom10_1',
+                      'A_2PLUS_mom_10_1',
+                      'A_2PLUS_mom110',
+                      'A_2PLUS_mom_110',
+                      'A_2PLUS_mom1_10',
+                      'A_2PLUS_mom_1_10',
+    ),
+    'A_2MINUS_mom11': ('A_2MINUS_mom011',
+                       'A_2MINUS_mom0_11',
+                       'A_2MINUS_mom01_1',
+                       'A_2MINUS_mom0_1_1',
+                       'A_2MINUS_mom101',
+                       'A_2MINUS_mom_101',
+                       'A_2MINUS_mom10_1',
+                       'A_2MINUS_mom_10_1',
+                       'A_2MINUS_mom110',
+                       'A_2MINUS_mom_110',
+                       'A_2MINUS_mom1_10',
+                       'A_2MINUS_mom_1_10',
+    ),
+    'B_mom111': ('B_0_mom111',
+                 'B_0_mom_1_1_1',
+                 'B_0_mom_111',
+                 'B_0_mom1_11',
+                 'B_0_mom11_1',
+                 'B_0_mom1_1_1',
+                 'B_0_mom_11_1',
+                 'B_0_mom_1_11'
+                 'B_1_mom111',
+                 'B_1_mom_1_1_1',
+                 'B_1_mom_111',
+                 'B_1_mom1_11',
+                 'B_1_mom11_1',
+                 'B_1_mom1_1_1',
+                 'B_1_mom_11_1',
+                 'B_1_mom_1_11'
+    ),
+    'A_1PLUS_avg_mom111': ('A_1PLUS_mom111',
+                           'A_1PLUS_mom_1_1_1',
+                           'A_1PLUS_mom_111',
+                           'A_1PLUS_mom1_11',
+                           'A_1PLUS_mom11_1',
+                           'A_1PLUS_mom1_1_1',
+                           'A_1PLUS_mom_11_1',
+                           'A_1PLUS_mom_1_11'
+    ),
+}
+
+for i in AVG_ROWS:
+    assert len(set(AVG_ROWS[i])) == len(list(AVG_ROWS[i])),\
+        "duplicate found:"+str(AVG_ROWS[i])
+    for j in AVG_ROWS[i]:
+        assert j in dir(cmod), "unknown irrep:"+str(j)
