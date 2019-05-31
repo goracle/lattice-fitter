33181fc83fd86bffe407609d4c6e54dc29934115
Dan H <goracle@users.noreply.github.com>
2019-05-31 16:24:43 -0400

    Add prelim commit for i1 moving frames.

---
+++ latfit/utilities/zeta/i1zeta.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+"""Get the phase shift for I1 moving frames"""
+import sys
+import numpy as np
+from czeta import czeta
+import latfit.utilities.read_file as rf
+
+COMP = 0.0
+L_BOX = 0.0
+IRREP = None
+MPION = 0.0
+
+def zetalm(l_arg, m_arg, qtwo, gamma):
+    """Compute generalized Luscher zeta function"""
+    zeta = czeta()
+    dlist = []
+    dx, dy, dz = COMP:
+    zeta.set_dgam(dx,dy,dz, gamma)
+    zeta.set_lm(l_arg, m_arg)
+    ret = zeta.evaluate(qtwo)
+    return ret
+
+def main():
+    """Get the phase shift for I1 moving frames
+    arglist = [binpath, str(epipi), str(PION_MASS), str(lbox),
+            str(comp[0]), str(comp[1]), str(comp[2]), str(gamma),
+            str(int(not FIT_SPACING_CORRECTION))]
+    """
+    this = sys.modules[__name__]
+    assert len(sys.argv) == 6, "needs: epipi, mpion, lbox, com units, irrep"
+    epipi = sys.argv[1]
+    setattr(this, 'MPION', np.float(sys.argv[2]))
+    setattr(this, 'L_BOX', np.float(sys.argv[3]))
+    setattr(this, 'COMP', rf.procmom(sys.argv[4]))
+    setattr(this, 'IRREP', str(sys.argv[5]))
+    return phase(epipi)
+
+
+class wfun:
+    """Compute wlm"""
+    def __init__(self):
+        """init"""
+        self.lbox = L_BOX
+        self._kmom = None
+        self._gamma = 1.0
+        self.qarg = None
+        self.cache = {}
+
+    @property
+    def kmom(self):
+        """Remember the energy"""
+        return self._kmom
+
+    @kmom.setter
+    def kmom(self, kmom):
+        """set the k"""
+        self._kmom = kmom
+        self.qarg = (self.kmom*self.lbox/np.pi/2)
+
+    def wfun(self, l_arg, m_arg):
+        """computes w_lm, l_arg==l, m_arg==m"""
+        key = (l_arg, m_arg)
+        if key in self.cache:
+            ret = self.cache[key]
+        else:
+            zlm = zetalm(l_arg, m_arg, self.qarg^2, self.gamma)
+            denom = np.pi**(3/2)*np.sqrt(2*l_arg+1)
+            denom *= self.gamma*self.qarg**(l_arg+1)
+            denom = np.complex(denom)
+            ret = zlm/denom
+            if not ret:
+                print("alert! w_"+str(l_arg)+","+str(m_arg)+" = 0")
+            self.cache[key] = ret
+        return ret
+
+    @property
+    def gamma(self):
+        """rel gamma"""
+        return self._gamma
+
+    @gamma.setter
+    def gamma(self, gamma):
+        """gamma setter"""
+        self._gamma = gamma
+
+def computek(epipi):
+    """Get the k (relative pipi momentum for zeta)"""
+    return np.sqrt(epipi*epipi/4 - MPION**2)
+
+def computegamma(epipi):
+    """Compute relativistic gamma given energy"""
+    arg = epipi**2-(2*np.pi/L_BOX)**2*comp**2
+    ret = epipi/np.sqrt(arg)
+    return ret
+
+def phase(epipi):
+    """Get the phase shift in degrees for I=1 moving frame"""
+    assert COMP, "center of mass momentum not set"
+    assert L_BOX, "box length not set"
+    assert MPION, "mass of pion not set"
+
+    # set up wlm
+    wlm = wfun()
+    wlm.kmom = computek(epipi)
+    wlm.gamma = computegamma(epipi)
+
+    cot = getcot(wlm)
+    tan = 1/cot
+    ret = np.arctan(tan)*180/np.pi
+    return ret
+
+def getcot(wlm):
+    """Get cotangent(phase shift)"""
+    assert IRREP is not None, "irrep not set"
+
+    # initial starting value to cotangent
+    cot = wlm(0,0)
+    units = np.sum(np.abs(COMP))
+
+    if units == 1:
+        if IRREP == 'A2':
+            cot += 2*wlm(2,0)
+        if IRREP == 'E':
+            cot += -wlm(2,0)
+    if units == 2:
+        if IRREP == 'B1':
+            cot += wlm(2,0)/2
+            cot += 1j*np.sqrt(6)*wlm(2,1)-sqrt(3/2)*wlm(2,2)
+        if IRREP == 'B2':
+            cot += wlm(2,0)/2
+            cot += -1j*np.sqrt(6)*wlm(2,1)-sqrt(3/2)*wlm(2,2)
+        if IRREP == 'B3':
+            cot += wlm(2,0)+sqrt(6)*wlm(2,2)
+    if units == 3:
+        if IRREP == 'A2':
+            cot += -1j*wlm(2,2)*np.sqrt(8/3)
+            cot += np.real(wlm(2,1))*np.sqrt(8/3)
+            cot += np.imag(wlm(2,1))*np.sqrt(8/3)
+        elif IRREP == 'E':
+            cot += 1j*np.sqrt(6)*wlm(2,2)
+    return cot
+
+if __name__ == '__main__':
+    main()
