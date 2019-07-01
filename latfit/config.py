98eef6e10ea97093ea9cb3231da41ba6a6f58b1e
Dan H <goracle@users.noreply.github.com>
2019-07-01 17:31:24 -0400

    Fix check bug.

--- latfit/config.py
+++ latfit/config.py
@@ -178,7 +178,8 @@ if LATTICE_ENSEMBLE == '24c':
     TSEP_VEC = [3 for _ in range(DIM)] if GEVP else [0]
 if LATTICE_ENSEMBLE == '32c':
     TSEP_VEC = [4 for _ in range(DIM)] if GEVP else [0]
-assert check_ids()[0] == TSEP_VEC[0], "ensemble mismatch:"+str(check_ids()[0])
+if GEVP:
+    assert check_ids()[0] == TSEP_VEC[0], "ensemble mismatch:"+str(check_ids()[0])
 
 # halve the data to check for consistencies
 HALF = 'first half'
