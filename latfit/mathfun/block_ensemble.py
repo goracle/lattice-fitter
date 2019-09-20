5fceec977ce5b1b85e9a06596657b95e7c1028c0
Dan H <goracle@users.noreply.github.com>
2019-09-20 13:07:44 -0400

    Add attempt to fix the weird bootstrap statistics I'm seeing.

--- latfit/mathfun/block_ensemble.py
+++ latfit/mathfun/block_ensemble.py
@@ -1,27 +1,47 @@
 """Block the ensemble by dropping JACKKNIFE_BLOCK_SIZE configs"""
 
+import sys
 import numpy as np
 from latfit.config import JACKKNIFE_BLOCK_SIZE
 from latfit.extract.inverse_jk import inverse_jk
 from latfit.utilities import exactmean as em
 from latfit.utilities.postprod.h5jack import dojackknife
+from latfit.utilities import exactmean as em
 import latfit.config
 
-def bootstrap_ensemble(reuse_inv, avg, reuse_blocked):
+def bootstrap_ensemble(reuse_inv, avg, reuse_blocked, config_num):
     """Generate a bootstrapped version of the ensemble
     with replacement, then jackknife it
     """
     if latfit.config.BOOTSTRAP:
+        reuse_inv = np.asarray(reuse_inv)
         choices = list(range(len(reuse_inv)))
-        retblk = []
+        retblk = np.zeros(reuse_inv.shape, dtype=reuse_inv.dtype)
+        idx = 0
         for _ in choices:
-            elem = reuse_inv[np.random.choice(choices)]
-            retblk.append(elem)
+            block = len(reuse_inv)-5
+            if idx+block > len(reuse_inv):
+                block = len(reuse_inv)-idx
+            #choice = np.random.randint(0, len(reuse_inv)-block)
+            choice = 0 if len(reuse_inv) == block else np.random.randint(
+                0, len(reuse_inv)-block)
+            for j in range(block):
+                retblk[idx+j] = reuse_inv[choice+j]
+            idx += block
+        for i, item in enumerate(retblk):
+            assert np.all(item != 0), str(item)+" "+str(i)
         ret = np.array(retblk, dtype=reuse_inv.dtype)
+        mean = em.acmean(ret, axis=0)
         ret = dojackknife(ret)
-        mean = np.mean(ret, axis=0)
+        assert np.allclose(mean, em.acmean(ret, axis=0), rtol=1e-12)
+        #assert np.mean(np.delete(reuse_inv, config_num, axis=0), axis=0) == avg[:,1], str(avg[:,1])+" "+str(np.mean(np.delete(reuse_inv, config_num, axis=0), axis=0))
+        avg = np.asarray(avg)
+        assert avg.shape[0] == mean.shape[0], "time extent does not match"
         avg = [[avg[i][0], mean[i]] for i, _ in enumerate(mean)]
-        avg = np.array(avg)
+        avg = np.asarray(avg)
+        #print(avg)
+        #print(mean)
+        #print(avg)
     else:
         ret = reuse_blocked
     return ret, avg
