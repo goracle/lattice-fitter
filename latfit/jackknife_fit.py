67f18a07cc74094d0c912b415f804ae172a8fe6b
Dan H <goracle@users.noreply.github.com>
2019-10-15 14:30:54 -0400

    Fix bug.

--- latfit/jackknife_fit.py
+++ latfit/jackknife_fit.py
@@ -198,12 +198,12 @@ elif JACKKNIFE_FIT == 'DOUBLE' or JACKKNIFE_FIT == 'SINGLE':
 
             # minimize chi^2 (t^2) given the inv. covariance matrix and data
             result_min_jack = mkmin.mkmin(covinv_jack, coords_jack)
-            mkmin.SPARAMS = result_min_jack.x
             if result_min_jack.status != 0:
                 assert not np.isnan(result_min_jack.status),\
                     str(result_min_jack.status)
                 result_min.misc.status = result_min_jack.status
                 raise NoConvergence
+            mkmin.SPARAMS = result_min_jack.x
             result_min.min_params.arr[config_num] = result_min_jack.x
 
             # store results for this fit
