d30582f79fd07122051465ecb9a880a9e11a767e
Dan H <goracle@users.noreply.github.com>
2020-01-13 15:34:43 -0500

    Add diagnostic.

--- latfit/utilities/combine_fit_results.sh
+++ latfit/utilities/combine_fit_results.sh
@@ -2,8 +2,14 @@
 
 #quantity_of_interest=$1
 combine_pickle.py $(ls energy*.p | grep -v err)
+echo "finished: energy"
 combine_pickle.py $(ls energy*.p | grep err)
+echo "finished: energy err"
 combine_pickle.py $(ls pvalue*.p | grep err)
+echo "finished: pvalue err"
 combine_pickle.py $(ls pvalue*.p | grep -v err)
+echo "finished: pvalue"
 combine_pickle.py $(ls phase*.p | grep -v err)
+echo "finished: phase shift"
 combine_pickle.py $(ls phase*.p | grep err)
+echo "finished: phase shift err"
