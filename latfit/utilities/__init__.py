5b8b0934603e27494bd4d0b641c7b5fea0b93be0
Dan H <goracle@users.noreply.github.com>
2020-01-18 09:35:51 -0500

    Update dunder init.

--- latfit/utilities/__init__.py
+++ latfit/utilities/__init__.py
@@ -1,5 +1,5 @@
 """Utilities to prepare production data for fitting."""
-__all__ = ['jk_make', 'op_compose', 'postprod',
-           'sum_blks', 'combine', 'proc_file',
+__all__ = ['jk_make', 'op_compose', 'postprod', 'postfit',
+           'sum_blks', 'combine', 'proc_file', 'tuplize',
            'aux_write', 'read_file', 'rho_pol_coeffs',
            'write_discon', 'traj_list', 'avgvac']
