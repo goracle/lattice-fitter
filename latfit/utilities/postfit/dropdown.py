3a75f826404723ac74f7560316889be8ec5db333
Dan H <goracle@users.noreply.github.com>
2020-04-01 14:43:35 -0400

    Add prelim statistics code for improved walk-back.
    (Not linked to anything yet; theory needs verification, testing).

---
+++ latfit/utilities/postfit/dropdown.py
@@ -0,0 +1,100 @@
+"""Calculate weight sum for list of best fits
+
+We "drop down" onto the plateau.
+The first point we land on is our answer
+(the first point on the plateau)
+"""
+
+import sys
+from scipy import stats
+import numpy as np
+from latfit.utilities import exactmean as em
+from latfit.analysis.superjack import jack_mean_err
+from latfit
+
+
+# the weight sum
+
+def weight_sum(sorted_block_list):
+    """Calculate the weight sum
+    return the weighted average
+    use list of jackknife blocks
+    index runs towards earlier (more goal-like) times
+    """
+    retblk = []
+    norm = [] # sum of weights
+    for idx, blk in enumerate(sorted_block_list):
+        nlist = sorted_block_list[:idx]
+        np1list = sorted_block_list[:idx+1]
+        fact1 = chained_probs(nlist)
+        fact2 = 1-chained_probs(np1list)
+        weight = fact1*fact2
+        norm.append(weight)
+        retblk.append(blk*weight)
+    # perform the weight sum here
+    norm = np.array(norm)
+    retblk = np.array(retblk)
+    retblk = em.acsum(retblk, axis=0)*em.acsum(1/norm, axis=0)
+
+    # jackknife average
+    mean, err = jack_mean_err(retblk)
+    return mean, err
+
+def chained_probs(nlist):
+    """Get the probablity that all blocks in nlist
+    have the same mean.
+    return a jackknifed answer to that question.
+    """
+    ret = 1
+    for idx, blk1 in enumerate(nlist):
+        for jdx, blk2 in enumerate(nlist):
+            if jdx <= idx:
+                continue
+            nprob = prob_blk(blk1, blk2)
+            nprob = np.asarray(nprob)
+            ret *= nprob
+    return ret
+
+
+def invert_jk(blk):
+    """Invert jackknife block"""
+    asum = em.acsum(blk, axis=0)
+    blk2 = blk*len(blk-1)
+    ret = asum-blk2
+    return ret
+
+def varidx(blk, idx):
+    """Calculate the variance of block with index idx
+    deleted"""
+    invblk = invert_jk(blk)
+    dblk = np.delete(invblk, idx, axis=0)
+    ret = em.acstd(dblk, axis=0, ddof=1)**2
+    return ret
+
+def tstat(blk1, blk2, idx):
+    """Calculate the sample statistic
+    Dependent t-test for paired samples
+    (correlated)
+    """
+    avg1 = blk1[idx]
+    avg2 = blk2[idx]
+    avg = avg1-avg2
+    var = varidx(blk1-blk2, idx)
+    lblk = len(blk1)
+    ret = avg
+    ret /= np.sqrt(var/lblk)
+    dof = lblk-1
+    return ret, dof
+
+
+def prob_blk(blk1, blk2):
+    """Calculate probablity that the two blocks are equal"""
+    res = []
+    assert len(blk1) == len(blk2), (len(blk1), len(blk2))
+    lblk = len(blk1)
+    for idx in range(lblk):
+        tval, dof = tstat(blk1, blk2, idx)
+        pval = 1 - stats.t.cdf(tval, dof)
+        res.append(pval)
+    return res
+
