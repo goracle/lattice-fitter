6559dc186f84736249e37829db1225e4181a1c67
Dan H <goracle@users.noreply.github.com>
2020-11-24 15:53:12 -0500

    Fix bugs.

--- latfit/utilities/postfit/walkpy.py
+++ latfit/utilities/postfit/walkpy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""apply walkback procedure to user inputted lists"""
 import sys
 from collections import defaultdict
 import os
@@ -14,13 +14,34 @@ def proc_walkres_input():
     args = sys.argv[1:]
     ret = []
     temp = ""
+    mdim = np.inf
     for i in args:
         if i != '@':
             temp += i
         else:
-            ret.append(ast.literal_eval(temp))
+            toapp = ast.literal_eval(temp)
+            toapp = filter_none(toapp)
+            mdim = min(len(toapp), mdim)
+            ret.append(toapp)
             temp = ""
+    ret2 = []
+    for app in ret:
+        if len(app) > mdim:
+            app = app[:mdim]
+        ret2.append(app)
+    return ret2
+
+def filter_none(toapp):
+    """get rid of everything beyond None in list"""
+    ret = []
+    for idx, item in enumerate(toapp):
+        it1, it2 = item
+        if idx or it1 == 'None': # p0 grd phase shift exception
+            if it1 == 'None' or it2 == 'None':
+                break
+        ret.append(item)
     return ret
+
 
 def gdoublestr(dstr):
     """convert string to gvar obj"""
@@ -56,17 +77,29 @@ def consis(it1, it2):
     ret = True if diff <= merr * 1.5 else False
     return ret
 
+def consis_list(it1, clist):
+    """Check it1 for consistency with list of items"""
+    return np.all([consis(it1, item) for item in clist])
+
 def walkback(eplist, reverse=False):
     """perform the (directed) walkback procedure on the phases"""
     ret = {}
     for lvl in eplist:
-        slist = sorted(eplist[lvl], reverse=reverse, key=lambda x: (x is None, x))
+        # sort such that 'None' phase shifts appear first
+        slist = sorted(eplist[lvl], reverse=reverse,
+                       key=lambda x: (x is None, x))
+        # we start walk-back with first list item, then walk
+        # towards higher indices
+        # direction of walk determined by
+        # list sort param 'reverse'
         mmin = slist[0]
+        clist = [mmin]
         for it1 in slist:
             if mmin is None:
                 continue
-            if mmin.sdev > it1.sdev and consis(it1, mmin):
+            if mmin.sdev > it1.sdev and consis_list(it1, clist):
                 mmin = it1
+                clist.append(mmin)
         ret[lvl] = mmin
     return ret
 
@@ -82,15 +115,13 @@ def main():
     """apply walkback procedure to user inputted lists"""
     enlist, phlist = readin()
     enlist = walkback(enlist, reverse=False)
-    reverse = False
-    if ISOSPIN == 2 or not ISOSPIN:
-        reverse = True
+    reverse = True if ISOSPIN == 2 or not ISOSPIN else False
     phlist = walkback(phlist, reverse=reverse)
     ret = rezip(enlist, phlist)
     print(equals_str(), ret)
 
 def equals_str():
-    """Get string like p0_lat or p0_32c_lat from cwd"""
+    """Get string like 'p0_lat =' or 'p0_32c_lat =' from cwd"""
     arg = os.getcwd()
     mom = arg.split('p')[-1].split('/')[0]
     ret = 'p'+mom+'_'
